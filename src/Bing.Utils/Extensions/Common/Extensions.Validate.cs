738d262c0e43228e2fe7ba394423cf9e1f731702
jian玄冰 <jianxuanhuo1@126.com>
2018-03-12 00:15:30 +0800

    更新验证

--- src/Bing.Utils/Extensions/Common/Extensions.Validate.cs
+++ src/Bing.Utils/Extensions/Common/Extensions.Validate.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// ReSharper disable once CheckNamespace
 namespace Bing.Utils.Extensions
 {
     /// <summary>
---
+++ src/Bing.Utils/Randoms/IRandomGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing.Utils.Randoms
+{
+    /// <summary>
+    /// 随机数生成器
+    /// </summary>
+    public interface IRandomGenerator
+    {
+        string Generate();
+    }
+}
