00ee41c059ada8cf3d51cc94e4451fdafbb32062
jian玄冰 <jianxuanhuo1@126.com>
2018-03-13 00:20:02 +0800

    加入 验证操作类 加入 通用扩展 更新 Web操作

--- src/Bing.Utils/Extensions/Common/Extensions.Common.cs
+++ src/Bing.Utils/Extensions/Common/Extensions.Common.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 // ReSharper disable once CheckNamespace
@@ -76,6 +77,92 @@ namespace Bing.Utils.Extensions
             return Utils.Helpers.Str.Join(list, quotes, separator);
         }
 
+        #endregion
+
+        #region IsMatch(是否匹配正则表达式)
+        /// <summary>
+        /// 确定所指定的正则表达式在指定的输入字符串中是否找到了匹配项
+        /// </summary>
+        /// <param name="value">要搜索匹配项的字符串</param>
+        /// <param name="pattern">要匹配的正则表达式模式</param>
+        /// <returns>如果正则表达式找到匹配项，则为 true；否则，为 false</returns>
+        public static bool IsMatch(this string value, string pattern)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            return Regex.IsMatch(value, pattern);
+        }
+        /// <summary>
+        /// 确定所指定的正则表达式在指定的输入字符串中找到匹配项
+        /// </summary>
+        /// <param name="value">要搜索匹配项的字符串</param>
+        /// <param name="pattern">要匹配的正则表达式模式</param>
+        /// <param name="options">规则</param>
+        /// <returns>如果正则表达式找到匹配项，则为 true；否则，为 false</returns>
+        public static bool IsMatch(this string value, string pattern, RegexOptions options)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            return Regex.IsMatch(value, pattern, options);
+        }
+        #endregion
+
+        #region GetMatch(获取匹配项)
+        /// <summary>
+        /// 在指定的输入字符串中搜索指定的正则表达式的第一个匹配项
+        /// </summary>
+        /// <param name="value">要搜索匹配项的字符串</param>
+        /// <param name="pattern">要匹配的正则表达式模式</param>
+        /// <returns>一个对象，包含有关匹配项的信息</returns>
+        public static string GetMatch(this string value, string pattern)
+        {
+            if (value.IsEmpty())
+            {
+                return string.Empty;
+            }
+            return Regex.Match(value, pattern).Value;
+        }
+
+        /// <summary>
+        /// 在指定的输入字符串中搜索指定的正则表达式的所有匹配项的字符串集合
+        /// </summary>
+        /// <param name="value">要搜索匹配项的字符串</param>
+        /// <param name="pattern">要匹配的正则表达式模式</param>
+        /// <returns> 一个集合，包含有关匹配项的字符串值</returns>
+        public static IEnumerable<string> GetMatchingValues(this string value, string pattern)
+        {
+            if (value.IsEmpty())
+            {
+                return new string[] { };
+            }
+            return GetMatchingValues(value, pattern, RegexOptions.None);
+        }
+        /// <summary>
+        /// 使用正则表达式来确定一个给定的正则表达式模式的所有匹配的字符串返回的枚举
+        /// </summary>
+        /// <param name="value">输入字符串</param>
+        /// <param name="pattern">正则表达式</param>
+        /// <param name="options">比较规则</param>
+        /// <returns>匹配字符串的枚举</returns>
+        public static IEnumerable<string> GetMatchingValues(this string value, string pattern, RegexOptions options)
+        {
+            return from Match match in GetMatches(value, pattern, options) where match.Success select match.Value;
+        }
+        /// <summary>
+        /// 使用正则表达式来确定指定的正则表达式模式的所有匹配项
+        /// </summary>
+        /// <param name="value">值</param>
+        /// <param name="pattern">正则表达式</param>
+        /// <param name="options">比较规则</param>
+        /// <returns></returns>
+        public static MatchCollection GetMatches(this string value, string pattern, RegexOptions options)
+        {
+            return Regex.Matches(value, pattern, options);
+        }
         #endregion
     }
 }
--- src/Bing.Utils/Helpers/Web.cs
+++ src/Bing.Utils/Helpers/Web.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Bing.Utils.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 
 namespace Bing.Utils.Helpers
 {
@@ -29,6 +33,105 @@ namespace Bing.Utils.Helpers
         /// 宿主环境
         /// </summary>
         public static IHostingEnvironment Environment { get; set; }
+
+        #endregion
+
+        #region Url(请求地址)
+
+        /// <summary>
+        /// 请求地址
+        /// </summary>
+        public static string Url => HttpContext?.Request?.GetDisplayUrl();
+
+        #endregion
+
+        #region IP(客户端IP地址)
+
+        /// <summary>
+        /// 客户端IP地址
+        /// </summary>
+        // ReSharper disable once InconsistentNaming
+        public static string IP
+        {
+            get
+            {
+                var list = new[] {"127.0.0.1", "::1"};
+                var result = HttpContext?.Connection?.RemoteIpAddress.SafeString();
+                if (string.IsNullOrWhiteSpace(result) || list.Contains(result))
+                {
+                    result = GetLanIP();
+                }
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 获取局域网IP
+        /// </summary>
+        /// <returns></returns>
+        // ReSharper disable once InconsistentNaming
+        private static string GetLanIP()
+        {
+            foreach (var hostAddress in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (hostAddress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return hostAddress.ToString();
+                }
+            }
+
+            return string.Empty;
+        }
+
+        #endregion
+
+        #region Host(主机)
+
+        /// <summary>
+        /// 主机
+        /// </summary>
+        public static string Host => HttpContext == null ? Dns.GetHostName() : GetClientHostName();
+
+        /// <summary>
+        /// 获取Web客户端主机名
+        /// </summary>
+        /// <returns></returns>
+        private static string GetClientHostName()
+        {
+            var address = GetRemoteAddress();
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                return Dns.GetHostName();
+            }
+
+            var result = Dns.GetHostEntry(IPAddress.Parse(address)).HostName;
+            if (result == "localhost.localdomain")
+            {
+                result = Dns.GetHostName();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 获取远程地址
+        /// </summary>
+        /// <returns></returns>
+        private static string GetRemoteAddress()
+        {
+            return HttpContext?.Request?.Headers["HTTP_X_FORWARDED_FOR"] ??
+                   HttpContext?.Request?.Headers["REMOTE_ADDR"];
+        }
+
+        #endregion
+
+        #region Browser(浏览器)
+
+        /// <summary>
+        /// 浏览器
+        /// </summary>
+        public static string Browser => HttpContext?.Request?.Headers["User-Agent"];
 
         #endregion
 
