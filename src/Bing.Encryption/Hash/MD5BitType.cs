8d8e3f7bb219f22672978689d44e1f9451ccac1c
jian玄冰 <jianxuanhuo1@126.com>
2018-03-21 12:12:21 +0800

    MD5 加密

---
+++ src/Bing.Encryption/Hash/MD5BitType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bing.Encryption.Hash
+{
+    /// <summary>
+    /// MD5字节类型
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public enum MD5BitType
+    {
+        L16,
+        L32,
+        L64
+    }
+}
