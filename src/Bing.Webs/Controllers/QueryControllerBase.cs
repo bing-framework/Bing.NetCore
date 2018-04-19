f97398f5eaf64267e39214466e9a41cba76b546a
jian玄冰 <jianxuanhuo1@126.com>
2018-04-20 00:15:55 +0800

    重构CRUD控制器

--- src/Bing.Webs/Controllers/QueryControllerBase.cs
+++ src/Bing.Webs/Controllers/QueryControllerBase.cs
@@ -5,31 +5,42 @@ using System.Threading.Tasks;
 using Bing.Applications;
 using Bing.Applications.Dtos;
 using Bing.Datas.Queries;
+using Bing.Domains.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bing.Webs.Controllers
 {
+    /// <summary>
+    /// 查询控制器
+    /// </summary>
+    /// <typeparam name="TDto">数据传输对象类型</typeparam>
+    /// <typeparam name="TQuery">查询参数类型</typeparam>
     public abstract class QueryControllerBase<TDto, TQuery> : WebApiControllerBase
         where TQuery : IQueryParameter 
-        where TDto : class, IDto, new()
+        where TDto : IResponse, new()
     {
         /// <summary>
-        /// Crud服务
+        /// 查询服务
         /// </summary>
-        private readonly ICrudService<TDto, TQuery> _service;
+        private readonly IQueryService<TDto, TQuery> _service;
 
         /// <summary>
         /// 初始化一个<see cref="QueryControllerBase{TDto,TQuery}"/>类型的实例
         /// </summary>
-        /// <param name="service">Crud服务</param>
-        protected QueryControllerBase(ICrudService<TDto, TQuery> service)
+        /// <param name="service">查询服务</param>
+        protected QueryControllerBase(IQueryService<TDto, TQuery> service)
         {
             _service = service;
         }
 
         /// <summary>
-        /// 获取单个实例，调用范例：GET URL(/api/customers/1)
+        /// 获取单个实例
         /// </summary>
+        /// <remarks>
+        /// 调用范例：
+        /// GET
+        /// /api/customers/1
+        /// </remarks>
         /// <param name="id">标识</param>
         /// <returns></returns>
         [HttpGet("{id}")]
@@ -40,27 +51,75 @@ namespace Bing.Webs.Controllers
         }
 
         /// <summary>
-        /// 查询，调用范例：GET URL(/api/customers/query?name=a)
+        /// 分页查询
         /// </summary>
-        /// <param name="query">查询参数</param>
-        /// <returns></returns>
-        [HttpGet("Query")]
-        public virtual async Task<IActionResult> QueryAsync(TQuery query)
-        {
-            var result = await _service.QueryAsync(query);
-            return Success(result);
-        }
-
-        /// <summary>
-        /// 分页查询，调用范例：GET URL(/api/customers?name=a)
-        /// </summary>
+        /// <remarks>
+        /// 调用范例：
+        /// GET
+        /// /api/customers?name=a
+        /// </remarks>
         /// <param name="query">查询参数</param>
         /// <returns></returns>
         [HttpGet]
         public virtual async Task<IActionResult> PagerQueryAsync(TQuery query)
         {
+            PagerQueryBefore(query);
             var result = await _service.PagerQueryAsync(query);
-            return Success(result);
-        }        
+            return Success(ToPagerQueryResult(result));
+        }
+
+        /// <summary>
+        /// 分页查询前操作
+        /// </summary>
+        /// <param name="query">查询参数</param>
+        protected virtual void PagerQueryBefore(TQuery query)
+        {
+        }
+
+        /// <summary>
+        /// 转换分页查询结果
+        /// </summary>
+        /// <param name="result">分页查询结果</param>
+        /// <returns></returns>
+        protected virtual dynamic ToPagerQueryResult(PagerList<TDto> result)
+        {
+            return result;
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <remarks>
+        /// 调用范例：
+        /// GET
+        /// /api/customers/query?name=a
+        /// </remarks>
+        /// <param name="query">查询参数</param>
+        /// <returns></returns>
+        [HttpGet("Query")]
+        public virtual async Task<IActionResult> QueryAsync(TQuery query)
+        {
+            QueryBefore(query);
+            var result = await _service.QueryAsync(query);
+            return Success(ToQueryResult(result));
+        }
+
+        /// <summary>
+        /// 查询前操作
+        /// </summary>
+        /// <param name="query">查询参数</param>
+        protected virtual void QueryBefore(TQuery query)
+        {
+        }
+
+        /// <summary>
+        /// 转换查询结果
+        /// </summary>
+        /// <param name="result">查询结果</param>
+        /// <returns></returns>
+        protected virtual dynamic ToQueryResult(List<TDto> result)
+        {
+            return result;
+        }
     }
 }
