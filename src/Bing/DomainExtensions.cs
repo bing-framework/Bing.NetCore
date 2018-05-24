c064b66d268e1fe99b57dd916d11bb7a725fa2f4
jian玄冰 <jianxuanhuo1@126.com>
2018-05-24 11:16:58 +0800

    添加键列表比较器 添加实体列表比较器

---
+++ src/Bing/DomainExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bing
+{
+    /// <summary>
+    /// 领域实体扩展
+    /// </summary>
+    public static partial class DomainExtensions
+    {
+    }
+}
---
+++ src/Bing/Domains/Entities/ListCompareResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bing.Domains.Entities
+{
+    /// <summary>
+    /// 列表比较结果
+    /// </summary>
+    /// <typeparam name="TEntity">实体类型</typeparam>
+    /// <typeparam name="TKey">标识类型</typeparam>
+    public class ListCompareResult<TEntity,TKey> where TEntity:IKey<TKey>
+    {
+        /// <summary>
+        /// 创建列表
+        /// </summary>
+        public List<TEntity> CreateList { get; }
+
+        /// <summary>
+        /// 更新列表
+        /// </summary>
+        public List<TEntity> UpdateList { get; }
+
+        /// <summary>
+        /// 删除列表
+        /// </summary>
+        public List<TEntity> DeleteList { get; }
+
+        /// <summary>
+        /// 初始化一个<see cref="ListCompareResult{TEntity,TKey}"/>类型的实例
+        /// </summary>
+        /// <param name="createList">创建列表</param>
+        /// <param name="updateList">更新列表</param>
+        /// <param name="deleteList">删除列表</param>
+        public ListCompareResult(List<TEntity> createList, List<TEntity> updateList, List<TEntity> deleteList)
+        {
+            CreateList = createList;
+            UpdateList = updateList;
+            DeleteList = deleteList;
+        }
+    }
+}
