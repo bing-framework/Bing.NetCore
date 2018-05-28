55e9e35a3f2f1eb593859cbb334654b4ba11dc00
jian玄冰 <jianxuanhuo1@126.com>
2018-05-28 18:10:59 +0800

    修改 启动项

--- dbdesigner/Bing.DbDesigner.Api/Startup.cs
+++ dbdesigner/Bing.DbDesigner.Api/Startup.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Bing.Datas.EntityFramework.SqlServer;
+using Bing.DbDesigner.Data;
+using Bing.DbDesigner.Data.UnitOfWorks.SqlServer;
+using Bing.Events.Default;
+using Bing.Logs.NLog;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Bing.DbDesigner.Api
 {
@@ -35,7 +42,29 @@ namespace Bing.DbDesigner.Api
         /// </summary>
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            // 添加MVC服务
+            services.AddMvc(options =>
+            {
+
+            }).AddControllersAsServices();
+
+            // 添加NLog日志操作
+            services.AddNLog();
+
+            // 添加事件总线服务
+            services.AddDefaultEventBus();
+
+            // 添加工作单元
+            services.AddSqlServerUnitOfWork<IDbDesignerUnitOfWork, DbDesignerUnitOfWork>(
+                Configuration.GetConnectionString("DefaultConnection"));
+
+            // 添加Swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info() {Title = "Bing数据库设计系统", Version = "v1"});
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Bing.DbDesigner.Api.xml"));
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Bing.DbDesigner.Service.xml"));
+            });
 
             // 添加Bing基础设施服务
             return services.AddBing();
