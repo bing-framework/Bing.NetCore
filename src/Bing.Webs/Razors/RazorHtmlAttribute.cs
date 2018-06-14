5e10a21e14f37aa201da44dfcd4af6c9adca3234
jian玄冰 <jianxuanhuo1@126.com>
2018-06-14 14:38:27 +0800

    实现 Razor生成Html

---
+++ src/Bing.Webs/Razors/RazorHtmlAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bing.Webs.Razors
+{
+    /// <summary>
+    /// Razor生成Html静态文件
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method,AllowMultiple = false, Inherited = false)]
+    public class RazorHtmlAttribute:Attribute
+    {
+        /// <summary>
+        /// 生成路径，相对根路径，范例：/Typings/app/app.component.html
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// 路径模板，范例：Typings/app/{area}/{controller}/{controller}-{action}.component.html
+        /// </summary>
+        public string Template { get; set; }
+    }
+}
