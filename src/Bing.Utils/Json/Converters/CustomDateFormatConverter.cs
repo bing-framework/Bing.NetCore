61f51480397529c8005099dd1deb4179774de0dc
jian玄冰 <jianxuanhuo1@126.com>
2018-07-02 13:35:21 +0800

    新增自定义时间格式转换器

---
+++ src/Bing.Utils/Json/Converters/CustomDateFormatConverter.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Converters;
+
+namespace Bing.Utils.Json.Converters
+{
+    /// <summary>
+    /// 自定义时间格式转换器
+    /// <para>
+    /// [JsonConverter(typeof(CustomDateFormatConverter),"yyyy-MM-dd HH:mm:ss")]
+    /// public DateTime Birthday { get; set; }
+    /// </para>
+    /// </summary>
+    public class CustomDateFormatConverter:IsoDateTimeConverter
+    {
+        /// <summary>
+        /// 初始化一个<see cref="CustomDateFormatConverter"/>类型的实例
+        /// </summary>
+        /// <param name="format">格式化字符串</param>
+        public CustomDateFormatConverter(string format)
+        {
+            this.DateTimeFormat = format;
+        }
+    }
+}
