eb45eb99c3e67e600afea2256b3f3fa611b92baf
jian玄冰 <jianxuanhuo1@126.com>
2018-07-06 18:06:20 +0800

    更新 状态码

--- src/Bing.Webs/Commons/StateCode.cs
+++ src/Bing.Webs/Commons/StateCode.cs
@@ -15,11 +15,16 @@ namespace Bing.Webs.Commons
         /// 成功
         /// </summary>
         [Description("成功")]
-        Ok=1,
+        Ok = 1,
         /// <summary>
         /// 失败
         /// </summary>
         [Description("失败")]
-        Fail=2
+        Fail = 2,
+        /// <summary>
+        /// 尚未登录
+        /// </summary>
+        [Description("尚未登录")]
+        NotLogin = 401
     }
 }
