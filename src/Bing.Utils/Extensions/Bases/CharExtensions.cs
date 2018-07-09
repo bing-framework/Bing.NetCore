5706520d2c15e5fc70482f78f84c01b0cc7dd413
jian玄冰 <jianxuanhuo1@126.com>
2018-07-09 10:10:49 +0800

    新增 char 扩展

---
+++ src/Bing.Utils/Extensions/Bases/CharExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace Bing.Utils.Extensions
+{
+    /// <summary>
+    /// 字符(<see cref="Char"/>) 扩展
+    /// </summary>
+    public static class CharExtensions
+    {
+        #region In(判断当前字符是否在目标字符数组中)
+
+        /// <summary>
+        /// 判断当前字符是否在目标字符数组中
+        /// </summary>
+        /// <param name="this">字符</param>
+        /// <param name="values">字符数组</param>
+        /// <returns></returns>
+        public static bool In(this char @this, params char[] values)
+        {
+            return Array.IndexOf(values, @this) != -1;
+        }
+
+        #endregion
+
+        #region NotIn(判断当前字符是否不在目标字符数组中)
+
+        /// <summary>
+        /// 判断当前字符是否不在目标字符数组中
+        /// </summary>
+        /// <param name="this">字符</param>
+        /// <param name="values">字符数组</param>
+        /// <returns></returns>
+        public static bool NotIn(this char @this, params char[] values)
+        {
+            return Array.IndexOf(values, @this) == -1;
+        }
+
+        #endregion
+
+        #region Repeat(指定次数重复一个字符)
+
+        /// <summary>
+        /// 指定次数重复一个字符
+        /// </summary>
+        /// <param name="this">字符</param>
+        /// <param name="repeatCount">重复数</param>
+        /// <returns></returns>
+        public static string Repeat(this char @this, int repeatCount)
+        {
+            return new string(@this, repeatCount);
+        }
+
+        #endregion
+    }
+}
