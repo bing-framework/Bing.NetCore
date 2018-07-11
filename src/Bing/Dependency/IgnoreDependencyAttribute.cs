be8d8cc920e778e578f5bfd3ba6c44a8b4c309a1
jian玄冰 <jianxuanhuo1@126.com>
2018-07-11 22:20:42 +0800

    新增 压缩操作类

---
+++ src/Bing/Dependency/IgnoreDependencyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bing.Dependency
+{
+    /// <summary>
+    /// 标注了此特性的类，将忽略依赖注入自动映射
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+    public class IgnoreDependencyAttribute : Attribute
+    {
+    }
+}
