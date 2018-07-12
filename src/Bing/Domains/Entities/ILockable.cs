59cdc04215739d2112b65a42219601a1d49fa103
jian玄冰 <jianxuanhuo1@126.com>
2018-07-12 21:40:46 +0800

    新增 数据计算操作类

---
+++ src/Bing/Domains/Entities/ILockable.cs
@@ -0,0 +1,13 @@
+﻿namespace Bing.Domains.Entities
+{
+    /// <summary>
+    /// 数据锁定
+    /// </summary>
+    public interface ILockable
+    {
+        /// <summary>
+        /// 是否锁定当前信息
+        /// </summary>
+        bool IsLocked { get; set; }
+    }
+}
