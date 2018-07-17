914f3df6d6650e53d8e7a8a1fa0e1e440353d72d
jian玄冰 <jianxuanhuo1@126.com>
2018-07-17 23:57:36 +0800

    更新类型扩展

--- src/Bing.Utils/Extensions/Common/Extensions.Type.cs
+++ src/Bing.Utils/Extensions/Common/Extensions.Type.cs
@@ -11,6 +11,18 @@ namespace Bing.Utils.Extensions
     /// </summary>
     public static partial class Extensions
     {
+        #region IsNullableType(是否可空类型)
+
+        /// <summary>
+        /// 是否可空类型
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <returns></returns>
+        public static bool IsNullableType(this Type type)
+        {
+            return ((type != null) && type.IsGenericType) && (type.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+
         /// <summary>
         /// 是否可空类型
         /// </summary>
@@ -27,6 +39,10 @@ namespace Bing.Utils.Extensions
             return genericParameterType == Nullable.GetUnderlyingType(type);
         }
 
+        #endregion
+
+        #region IsNullableEnum(是否可空枚举类型)
+
         /// <summary>
         /// 是否可空枚举类型
         /// </summary>
@@ -36,6 +52,10 @@ namespace Bing.Utils.Extensions
         {
             return Nullable.GetUnderlyingType(type)?.GetTypeInfo().IsEnum ?? false;
         }
+
+        #endregion
+
+        #region HasAttribute(是否有指定特性)
 
         /// <summary>
         /// 是否有指定特性
@@ -49,6 +69,10 @@ namespace Bing.Utils.Extensions
             return type.GetTypeInfo().IsDefined(typeof(T), inherit);
         }
 
+        #endregion
+
+        #region GetAttributes(获取指定特性集合)
+
         /// <summary>
         /// 获取指定特性集合
         /// </summary>
@@ -61,6 +85,10 @@ namespace Bing.Utils.Extensions
             return type.GetTypeInfo().GetCustomAttributes<T>(inherit);
         }
 
+        #endregion
+
+        #region GetAttribute(获取指定特性)
+
         /// <summary>
         /// 获取指定特性
         /// </summary>
@@ -72,6 +100,80 @@ namespace Bing.Utils.Extensions
         {
             return type.GetTypeInfo().GetCustomAttributes<T>(inherit).FirstOrDefault();
         }
+
+        #endregion
+
+        #region IsCustomType(是否自定义类型)
+
+        /// <summary>
+        /// 是否自定义类型
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <returns></returns>
+        public static bool IsCustomType(this Type type)
+        {
+            if (type.IsPrimitive)
+            {
+                return false;
+            }
+
+            if (type.IsArray && type.HasElementType && type.GetElementType().IsPrimitive)
+            {
+                return false;
+            }
+
+            return type != typeof(object) && type != typeof(Guid) &&
+                   Type.GetTypeCode(type) == TypeCode.Object && !type.IsGenericType;
+        }
+
+        #endregion
+
+        #region IsAnonymousType(是否匿名类型)
+
+        /// <summary>
+        /// 是否匿名类型
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <returns></returns>
+        public static bool IsAnonymousType(this Type type)
+        {
+            const string csharpAnonPrefix = "<>f__AnonymousType";
+            const string vbAnonPrefix = "VB$Anonymous";
+            var typeName = type.Name;
+            return typeName.StartsWith(csharpAnonPrefix) || typeName.StartsWith(vbAnonPrefix);
+        }
+
+        #endregion
+
+        #region IsBaseType(是否基类型)
+
+        /// <summary>
+        /// 是否基类型
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="checkingType">检查类型</param>
+        /// <returns></returns>
+        public static bool IsBaseType(this Type type, Type checkingType)
+        {
+            while (type!=typeof(object))
+            {
+                if (type == null)
+                {
+                    continue;
+                }
+
+                if (type == checkingType)
+                {
+                    return true;
+                }
+
+                type = type.BaseType;
+            }
+
+            return false;
+        }
+
+        #endregion
 
         /// <summary>
         /// 能否用于数据库存储
@@ -98,39 +200,5 @@ namespace Bing.Utils.Extensions
                    || type.GetTypeInfo().IsEnum
                    || Nullable.GetUnderlyingType(type) != null && CanUseForDb(Nullable.GetUnderlyingType(type));
         }
-
-        /// <summary>
-        /// 是否自定义类型
-        /// </summary>
-        /// <param name="type">类型</param>
-        /// <returns></returns>
-        public static bool IsCustomType(this Type type)
-        {
-            if (type.IsPrimitive)
-            {
-                return false;
-            }
-
-            if (type.IsArray && type.HasElementType && type.GetElementType().IsPrimitive)
-            {
-                return false;
-            }
-
-            return type != typeof(object) && type != typeof(Guid) &&
-                   Type.GetTypeCode(type) == TypeCode.Object && !type.IsGenericType;
-        }
-
-        /// <summary>
-        /// 是否匿名类型
-        /// </summary>
-        /// <param name="type">类型</param>
-        /// <returns></returns>
-        public static bool IsAnonymousType(this Type type)
-        {
-            const string csharpAnonPrefix = "<>f__AnonymousType";
-            const string vbAnonPrefix = "VB$Anonymous";
-            var typeName = type.Name;
-            return typeName.StartsWith(csharpAnonPrefix) || typeName.StartsWith(vbAnonPrefix);
-        }        
     }
 }
