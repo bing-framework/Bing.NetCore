3b4dbe91eced6aad8232d596c0dc49255b33d72d
jian玄冰 <jianxuanhuo1@126.com>
2018-07-26 12:20:40 +0800

    新增 雪花算法单元测试

---
+++ tests/Bing.Sequence.Tests/TestBase.cs
@@ -0,0 +1,29 @@
+﻿using Bing.Sequence.Abstractions;
+using Bing.Sequence.Snowflake;
+using Bing.Utils.Develops;
+using Xunit.Abstractions;
+
+namespace Bing.Sequence.Tests
+{
+    public class TestBase
+    {
+        protected ITestOutputHelper Output;
+
+        public TestBase(ITestOutputHelper output)
+        {
+            Output = output;
+            UnitTester.WriteLine = Output.WriteLine;
+            CodeRamer.WriteLine = Output.WriteLine;
+            CodeTimer.WriteLine = Output.WriteLine;
+        }
+
+        /// <summary>
+        /// 获取雪花算法序列号生成器
+        /// </summary>
+        /// <returns></returns>
+        protected ISequence GetSnowflakeSequence()
+        {
+            return SnowflakeSequenceBuilder.Create(1, 2).Build();
+        }
+    }
+}
