44fb819f9b5545b40acdf2ed76155a4d5b1913d4
jian玄冰 <jianxuanhuo1@126.com>
2018-07-27 12:14:21 +0800

    新增 ElasticSearch 组件

---
+++ src/Bing.ElasticSearch/Configs/ElasticSearchConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace Bing.ElasticSearch.Configs
+{
+    /// <summary>
+    /// ElasticSearch 连接配置
+    /// </summary>
+    public class ElasticSearchConfig
+    {
+        /// <summary>
+        /// 连接字符串，支持多个节点主机，使用|进行分隔。
+        /// 例如：localhost:9200|localhost:8200
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ src/Bing.ElasticSearch/Configs/IElasticSearchConfigProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace Bing.ElasticSearch.Configs
+{
+    /// <summary>
+    /// ElasticSearch 配置提供器
+    /// </summary>
+    public interface IElasticSearchConfigProvider
+    {
+        /// <summary>
+        /// 获取配置
+        /// </summary>
+        /// <returns></returns>
+        ElasticSearchConfig GetConfig();
+
+        /// <summary>
+        /// 获取配置
+        /// </summary>
+        /// <returns></returns>
+        Task<ElasticSearchConfig> GetConfigAsync();
+    }
+}
