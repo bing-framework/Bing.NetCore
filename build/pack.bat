7f5950d7965227f5cd3b41ddc8fc11c24c70118b
jian玄冰 <jianxuanhuo1@126.com>
2018-09-20 17:35:44 +0800

    新增 批处理命令

---
+++ build/pack.bat
@@ -0,0 +1,45 @@
+@echo off
+echo %1
+cd ..
+
+rem clear old packages
+del output\* /q/f/s
+
+rem build
+dotnet build Bing.NetCore.sln -c Release
+
+rem pack
+dotnet pack ./src/Bing.Utils/Bing.Utils.csproj
+dotnet pack ./src/Bing/Bing.csproj
+dotnet pack ./src/Bing.Security/Bing.Security.csproj
+dotnet pack ./src/Bing.Logs/Bing.Logs.csproj
+dotnet pack ./src/Bing.Logs.Exceptionless/Bing.Logs.Exceptionless.csproj
+dotnet pack ./src/Bing.Logs.Log4Net/Bing.Logs.Log4Net.csproj
+dotnet pack ./src/Bing.Logs.NLog/Bing.Logs.NLog.csproj
+dotnet pack ./src/Bing.Logs.Serilog/Bing.Logs.Serilog.csproj
+dotnet pack ./src/Bing.Events/Bing.Events.csproj
+dotnet pack ./src/Bing.Datas.Dapper/Bing.Datas.Dapper.csproj
+dotnet pack ./src/Bing.Datas.EntityFramework/Bing.Datas.EntityFramework.csproj
+dotnet pack ./src/Bing.Datas.EntityFramework.MySql/Bing.Datas.EntityFramework.MySql.csproj
+dotnet pack ./src/Bing.Datas.EntityFramework.PgSql/Bing.Datas.EntityFramework.PgSql.csproj
+dotnet pack ./src/Bing.Datas.EntityFramework.SqlServer/Bing.Datas.EntityFramework.SqlServer.csproj
+dotnet pack ./src/Bing.Applications/Bing.Applications.csproj
+dotnet pack ./src/Bing.Webs/Bing.Webs.csproj
+dotnet pack ./src/Bing.Extensions.AutoMapper/Bing.Extensions.AutoMapper.csproj
+dotnet pack ./src/Bing.Extensions.Swashbuckle/Bing.Extensions.Swashbuckle.csproj
+dotnet pack ./src/Bing.Tools.QrCode/Bing.Tools.QrCode.csproj
+dotnet pack ./src/Bing.Tools.QrCode.QRCoder/Bing.Tools.QrCode.QRCoder.csproj
+dotnet pack ./src/Bing.Tools.QrCode.ZXing/Bing.Tools.QrCode.ZXing.csproj
+dotnet pack ./src/Bing.MailKit/Bing.MailKit.csproj
+dotnet pack ./src/Bing.Biz/Bing.Biz.csproj
+dotnet pack ./src/Bing.Encryption/Bing.Encryption.csproj
+dotnet pack ./src/Bing.Geetest/Bing.Geetest.csproj
+dotnet pack ./src/Bing.MockData/Bing.MockData.csproj
+dotnet pack ./src/Bing.BankCardInfo/Bing.BankCardInfo.csproj
+dotnet pack ./src/Bing.ElasticSearch/Bing.ElasticSearch.csproj
+dotnet pack ./src/Bing.Sequence/Bing.Sequence.csproj
+rem dotnet pack ./src/Bing.Offices/Bing.Offices.csproj
+rem dotnet pack ./src/Bing.Offices.Npoi/Bing.Offices.Npoi.csproj
+
+rem push
+for %%i in (output\release\*.nupkg) do dotnet nuget push %%i -k %1 -s https://www.nuget.org/api/v2/package
