357a52b061a80e940a06f5f6e99f89b6c2a43a1b
jian玄冰 <jianxuanhuo1@126.com>
2018-09-28 11:38:19 +0800

    新增 Offices Npoi 单元测试

---
+++ tests/Bing.Offices.Tests/Npoi/NpoiWorkbookTest.cs
@@ -0,0 +1,30 @@
+﻿using Bing.Offices.Excels.Abstractions;
+using Bing.Offices.Excels.Enums;
+using Bing.Offices.Npoi.Excels.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Bing.Offices.Tests.Npoi
+{
+    /// <summary>
+    /// Npoi 工作簿
+    /// https://github.com/EdwardLiulei/ExcelRW.git
+    /// </summary>
+    public class NpoiWorkbookTest:TestBase
+    {
+        public NpoiWorkbookTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Test_Save()
+        {
+            IWorkbook workbook = new NpoiWorkbook(ExcelFormat.Xlsx);
+            var sheet = workbook.InsertSheet("测试工作表");
+            sheet.InsertRow(1);
+            var cell = sheet.GetCell(1, 1);
+            cell.SetValue("测试数值");
+            workbook.Save("D:\\测试Npoi.xlsx");
+        }
+    }
+}
