ff0641edfacd4d6c4de7df54eb94e7b7613fdf86
jian玄冰 <jianxuanhuo1@126.com>
2018-10-10 16:07:41 +0800

    新增 程序集查找器

---
+++ src/Bing/Reflections/IAllAssemblyFinder.cs
@@ -0,0 +1,9 @@
+﻿namespace Bing.Reflections
+{
+    /// <summary>
+    /// 所有程序集查找器
+    /// </summary>
+    public interface IAllAssemblyFinder:IAssemblyFinder
+    {
+    }
+}
---
+++ src/Bing/Reflections/IAssemblyFinder.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using Bing.Finders;
+
+namespace Bing.Reflections
+{
+    /// <summary>
+    /// 程序集查找器
+    /// </summary>
+    public interface IAssemblyFinder : IFinder<Assembly>
+    {
+    }
+}
