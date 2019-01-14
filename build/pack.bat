7c2865b7b15b2852d05125652904a44b708a9457
jian玄冰 <jianxuanhuo1@126.com>
2019-01-14 22:11:48 +0800

    移除加密库

--- build/pack.bat
+++ build/pack.bat
@@ -29,9 +29,7 @@ dotnet pack ./src/Bing.Extensions.AutoMapper/Bing.Extensions.AutoMapper.csproj
 dotnet pack ./src/Bing.Extensions.Swashbuckle/Bing.Extensions.Swashbuckle.csproj
 dotnet pack ./src/Bing.MailKit/Bing.MailKit.csproj
 dotnet pack ./src/Bing.Biz/Bing.Biz.csproj
-dotnet pack ./src/Bing.Encryption/Bing.Encryption.csproj
 dotnet pack ./src/Bing.Geetest/Bing.Geetest.csproj
-dotnet pack ./src/Bing.BankCardInfo/Bing.BankCardInfo.csproj
 dotnet pack ./src/Bing.ElasticSearch/Bing.ElasticSearch.csproj
 dotnet pack ./src/Bing.Sequence/Bing.Sequence.csproj
 dotnet pack ./src/Bing.Caching.InMemory/Bing.Caching.InMemory.csproj
