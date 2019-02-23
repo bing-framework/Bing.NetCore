5252871ab86e54d323d53e9a7da717eb8a1ccff1
jian玄冰 <jianxuanhuo1@126.com>
2019-02-23 15:21:39 +0800

    更新 文件操作辅助类

---
+++ src/Bing.Utils/IO/FileUtil.Info.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Bing.Utils.Files;
+using Bing.Utils.Helpers;
+using FileInfo = System.IO.FileInfo;
+
+namespace Bing.Utils.IO
+{
+    /// <summary>
+    /// 文件操作辅助类 - 信息
+    /// </summary>
+    public static partial class FileUtil
+    {
+        #region GetContentType(根据扩展名获取文件内容类型)
+
+        /// <summary>
+        /// 根据扩展名获取文件内容类型
+        /// </summary>
+        /// <param name="ext">扩展名</param>
+        /// <returns></returns>
+        public static string GetContentType(string ext)
+        {
+            var dict = Const.FileExtensionDict;
+            ext = ext.ToLower();
+            if (!ext.StartsWith("."))
+            {
+                ext = "." + ext;
+            }
+
+            dict.TryGetValue(ext, out var contentType);
+            return contentType;
+        }
+
+        #endregion
+
+        #region GetFileSize(获取文件大小)
+
+        /// <summary>
+        /// 获取文件大小
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <returns></returns>
+        public static FileSize GetFileSize(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            return GetFileSize(new System.IO.FileInfo(filePath));
+        }
+
+        /// <summary>
+        /// 获取文件大小
+        /// </summary>
+        /// <param name="fileInfo">文件信息</param>
+        /// <returns></returns>
+        public static FileSize GetFileSize(FileInfo fileInfo)
+        {
+            if (fileInfo == null)
+            {
+                throw new ArgumentNullException(nameof(fileInfo));
+            }
+
+            return new FileSize(fileInfo.Length);
+        }
+
+        #endregion
+
+        #region GetVersion(获取文件版本号)
+
+        /// <summary>
+        /// 获取文件版本号
+        /// </summary>
+        /// <param name="fileName">完整文件名</param>
+        /// <returns></returns>
+        public static string GetVersion(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(fileName);
+                return fvi.FileVersion;
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region GetFileMd5(获取文件的MD5值)
+
+        /// <summary>
+        /// 获取文件的MD5值
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns></returns>
+        public static string GetFileMd5(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            const int bufferSize = 1024 * 1024;
+            byte[] buffer = new byte[bufferSize];
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            md5.Initialize();
+
+            long offset = 0;
+            while (offset < fs.Length)
+            {
+                long readSize = bufferSize;
+                if (offset + readSize > fs.Length)
+                {
+                    readSize = fs.Length - offset;
+                }
+
+                fs.Read(buffer, 0, (int)readSize);
+                if (offset + readSize < fs.Length)
+                {
+                    md5.TransformBlock(buffer, 0, (int)readSize, buffer, 0);
+                }
+                else
+                {
+                    md5.TransformFinalBlock(buffer, 0, (int)readSize);
+                }
+
+                offset += bufferSize;
+            }
+
+            fs.Close();
+            byte[] result = md5.Hash;
+            md5.Clear();
+            StringBuilder sb = new StringBuilder();
+            foreach (var b in result)
+            {
+                sb.Append(b.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+        #region GetEncoding(获取文件编码)
+
+        /// <summary>
+        /// 获取文件编码
+        /// </summary>
+        /// <param name="filePath">文件绝对路径</param>
+        /// <returns></returns>
+        public static Encoding GetEncoding(string filePath)
+        {
+            return GetEncoding(filePath, Encoding.Default);
+        }
+
+        /// <summary>
+        /// 获取文件编码
+        /// </summary>
+        /// <param name="filePath">文件绝对路径</param>
+        /// <param name="defaultEncoding">默认编码</param>
+        /// <returns></returns>
+        public static Encoding GetEncoding(string filePath, Encoding defaultEncoding)
+        {
+            Encoding targetEncoding = defaultEncoding;
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4))
+            {
+                if (fs != null && fs.Length >= 2)
+                {
+                    long pos = fs.Position;
+                    fs.Position = 0;
+                    int[] buffer = new int[4];
+                    buffer[0] = fs.ReadByte();
+                    buffer[1] = fs.ReadByte();
+                    buffer[2] = fs.ReadByte();
+                    buffer[3] = fs.ReadByte();
+                    fs.Position = pos;
+
+                    if (buffer[0] == 0xFE && buffer[1] == 0xFF)
+                    {
+                        targetEncoding = Encoding.BigEndianUnicode;
+                    }
+
+                    if (buffer[0] == 0xFF && buffer[1] == 0xFE)
+                    {
+                        targetEncoding = Encoding.Unicode;
+                    }
+
+                    if (buffer[0] == 0xEF && buffer[1] == 0xBB && buffer[2] == 0xBF)
+                    {
+                        targetEncoding = Encoding.UTF8;
+                    }
+                }
+            }
+
+            return targetEncoding;
+        }
+
+        #endregion
+    }
+}
--- src/Bing.Utils/IO/FileUtil.cs
+++ src/Bing.Utils/IO/FileUtil.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bing.Utils.Extensions;
-using Bing.Utils.Files;
-using Bing.Utils.Helpers;
 using FileInfo = System.IO.FileInfo;
 
 namespace Bing.Utils.IO
@@ -17,7 +14,7 @@ namespace Bing.Utils.IO
     /// <summary>
     /// 文件操作辅助类
     /// </summary>
-    public static class FileUtil
+    public static partial class FileUtil
     {
         #region CreateIfNotExists(创建文件，如果文件不存在)
 
@@ -219,134 +216,6 @@ namespace Bing.Utils.IO
 
         #endregion
 
-        #region GetVersion(获取文件版本号)
-
-        /// <summary>
-        /// 获取文件版本号
-        /// </summary>
-        /// <param name="fileName">完整文件名</param>
-        /// <returns></returns>
-        public static string GetVersion(string fileName)
-        {
-            if (File.Exists(fileName))
-            {
-                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(fileName);
-                return fvi.FileVersion;
-            }
-
-            return null;
-        }
-
-        #endregion
-
-        #region GetFileMd5(获取文件的MD5值)
-
-        /// <summary>
-        /// 获取文件的MD5值
-        /// </summary>
-        /// <param name="fileName">文件名</param>
-        /// <returns></returns>
-        public static string GetFileMd5(string fileName)
-        {
-            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
-            const int bufferSize = 1024 * 1024;
-            byte[] buffer = new byte[bufferSize];
-
-            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-            md5.Initialize();
-
-            long offset = 0;
-            while (offset < fs.Length)
-            {
-                long readSize = bufferSize;
-                if (offset + readSize > fs.Length)
-                {
-                    readSize = fs.Length - offset;
-                }
-
-                fs.Read(buffer, 0, (int) readSize);
-                if (offset + readSize < fs.Length)
-                {
-                    md5.TransformBlock(buffer, 0, (int) readSize, buffer, 0);
-                }
-                else
-                {
-                    md5.TransformFinalBlock(buffer, 0, (int) readSize);
-                }
-
-                offset += bufferSize;
-            }
-
-            fs.Close();
-            byte[] result = md5.Hash;
-            md5.Clear();
-            StringBuilder sb = new StringBuilder();
-            foreach (var b in result)
-            {
-                sb.Append(b.ToString("X2"));
-            }
-
-            return sb.ToString();
-        }
-
-        #endregion
-
-        #region GetEncoding(获取文件编码)
-
-        /// <summary>
-        /// 获取文件编码
-        /// </summary>
-        /// <param name="filePath">文件绝对路径</param>
-        /// <returns></returns>
-        public static Encoding GetEncoding(string filePath)
-        {
-            return GetEncoding(filePath, Encoding.Default);
-        }
-
-        /// <summary>
-        /// 获取文件编码
-        /// </summary>
-        /// <param name="filePath">文件绝对路径</param>
-        /// <param name="defaultEncoding">默认编码</param>
-        /// <returns></returns>
-        public static Encoding GetEncoding(string filePath, Encoding defaultEncoding)
-        {
-            Encoding targetEncoding = defaultEncoding;
-            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4))
-            {
-                if (fs != null && fs.Length >= 2)
-                {
-                    long pos = fs.Position;
-                    fs.Position = 0;
-                    int[] buffer = new int[4];
-                    buffer[0] = fs.ReadByte();
-                    buffer[1] = fs.ReadByte();
-                    buffer[2] = fs.ReadByte();
-                    buffer[3] = fs.ReadByte();
-                    fs.Position = pos;
-
-                    if (buffer[0] == 0xFE && buffer[1] == 0xFF)
-                    {
-                        targetEncoding = Encoding.BigEndianUnicode;
-                    }
-
-                    if (buffer[0] == 0xFF && buffer[1] == 0xFE)
-                    {
-                        targetEncoding = Encoding.Unicode;
-                    }
-
-                    if (buffer[0] == 0xEF && buffer[1] == 0xBB && buffer[2] == 0xBF)
-                    {
-                        targetEncoding = Encoding.UTF8;
-                    }
-                }
-            }
-
-            return targetEncoding;
-        }
-
-        #endregion
-
         #region GetAllFiles(获取目录中全部文件列表)
 
         /// <summary>
@@ -361,40 +230,7 @@ namespace Bing.Utils.IO
 
         #endregion
 
-        #region GetContentType(根据扩展名获取文件内容类型)
-
-        /// <summary>
-        /// 根据扩展名获取文件内容类型
-        /// </summary>
-        /// <param name="ext">扩展名</param>
-        /// <returns></returns>
-        public static string GetContentType(string ext)
-        {
-            string contentType = "";
-            var dict = Const.FileExtensionDict;
-            ext = ext.ToLower();
-            if (!ext.StartsWith("."))
-            {
-                ext = "." + ext;
-            }
-
-            dict.TryGetValue(ext, out contentType);
-            return contentType;
-        }
-
-        #endregion
-
-        #region Read(读取文件到字符串)
-
-        /// <summary>
-        /// 读取文件到字符串
-        /// </summary>
-        /// <param name="filePath">文件的绝对路径</param>
-        /// <returns></returns>
-        public static string Read(string filePath)
-        {
-            return Read(filePath, Encoding.UTF8);
-        }
+        #region Read(读取文件到字符串)        
 
         /// <summary>
         /// 读取文件到字符串
@@ -402,8 +238,13 @@ namespace Bing.Utils.IO
         /// <param name="filePath">文件的绝对路径</param>
         /// <param name="encoding">字符编码</param>
         /// <returns></returns>
-        public static string Read(string filePath, Encoding encoding)
+        public static string Read(string filePath, Encoding encoding = null)
         {
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+
             if (!File.Exists(filePath))
             {
                 return string.Empty;
@@ -431,8 +272,22 @@ namespace Bing.Utils.IO
                 return null;
             }
 
-            FileInfo fileInfo = new FileInfo(filePath);
-            int fileSize = (int) fileInfo.Length;
+            return ReadToBytes(new FileInfo(filePath));
+        }
+
+        /// <summary>
+        /// 将文件读取到字节流中
+        /// </summary>
+        /// <param name="fileInfo">文件信息</param>
+        /// <returns></returns>
+        public static byte[] ReadToBytes(FileInfo fileInfo)
+        {
+            if (fileInfo == null)
+            {
+                return null;
+            }
+
+            int fileSize = (int)fileInfo.Length;
             using (BinaryReader reader = new BinaryReader(fileInfo.Open(FileMode.Open)))
             {
                 return reader.ReadBytes(fileSize);
@@ -471,217 +326,6 @@ namespace Bing.Utils.IO
             }
 
             File.WriteAllBytes(filePath, bytes);
-        }
-
-        #endregion
-
-        #region ToString(转换成字符串)        
-
-        /// <summary>
-        /// 字节数组转换成字符串
-        /// </summary>
-        /// <param name="data">数据,默认字符编码utf-8</param>
-        /// <returns></returns>
-        public static string ToString(byte[] data)
-        {
-            return ToString(data, Encoding.UTF8);
-        }
-
-        /// <summary>
-        /// 字节数组转换成字符串
-        /// </summary>
-        /// <param name="data">数据</param>
-        /// <param name="encoding">字符编码</param>
-        /// <returns></returns>
-        public static string ToString(byte[] data, Encoding encoding)
-        {
-            if (data == null || data.Length == 0)
-            {
-                return string.Empty;
-            }
-
-            return encoding.GetString(data);
-        }
-
-        /// <summary>
-        /// 流转换成字符串
-        /// </summary>
-        /// <param name="stream">流</param>
-        /// <param name="encoding">字符串编码</param>
-        /// <param name="bufferSize">缓冲区大小</param>
-        /// <param name="isCloseStream">读取完成是否释放流，默认为true</param>
-        /// <returns></returns>
-        public static string ToString(Stream stream, Encoding encoding = null, int bufferSize = 1024 * 2,
-            bool isCloseStream = true)
-        {
-            if (stream == null)
-            {
-                return string.Empty;
-            }
-
-            if (encoding == null)
-            {
-                encoding = Encoding.UTF8;
-            }
-
-            if (stream.CanRead == false)
-            {
-                return string.Empty;
-            }
-
-            using (var reader = new StreamReader(stream, encoding, true, bufferSize, !isCloseStream))
-            {
-                if (stream.CanSeek)
-                {
-                    stream.Seek(0, SeekOrigin.Begin);
-                }
-
-                var result = reader.ReadToEnd();
-                if (stream.CanSeek)
-                {
-                    stream.Seek(0, SeekOrigin.Begin);
-                }
-
-                return result;
-            }
-        }
-
-        /// <summary>
-        /// 流转换成字符串
-        /// </summary>
-        /// <param name="stream">流</param>
-        /// <param name="encoding">字符串编码</param>
-        /// <param name="bufferSize">缓冲区大小</param>
-        /// <param name="isCloseStream">读取完成是否释放流，默认为true</param>
-        /// <returns></returns>
-        public static async Task<string> ToStringAsync(Stream stream, Encoding encoding = null,
-            int bufferSize = 1024 * 2,
-            bool isCloseStream = true)
-        {
-            if (stream == null)
-            {
-                return string.Empty;
-            }
-
-            if (encoding == null)
-            {
-                encoding = Encoding.UTF8;
-            }
-
-            if (stream.CanRead == false)
-            {
-                return string.Empty;
-            }
-
-            using (var reader = new StreamReader(stream, encoding, true, bufferSize, !isCloseStream))
-            {
-                if (stream.CanSeek)
-                {
-                    stream.Seek(0, SeekOrigin.Begin);
-                }
-
-                var result = await reader.ReadToEndAsync();
-                if (stream.CanSeek)
-                {
-                    stream.Seek(0, SeekOrigin.Begin);
-                }
-
-                return result;
-            }
-        }
-
-        #endregion
-
-        #region ToStream(转换成流)
-
-        /// <summary>
-        /// 字符串转换成流
-        /// </summary>
-        /// <param name="data">数据</param>
-        /// <returns></returns>
-        public static Stream ToStream(string data)
-        {
-            return ToStream(data, Encoding.UTF8);
-        }
-
-        /// <summary>
-        /// 字符串转换成流
-        /// </summary>
-        /// <param name="data">数据</param>
-        /// <param name="encoding">字符编码</param>
-        /// <returns></returns>
-        public static Stream ToStream(string data, Encoding encoding)
-        {
-            if (string.IsNullOrWhiteSpace(data))
-            {
-                return Stream.Null;
-            }
-
-            return new MemoryStream(ToBytes(data, encoding));
-        }
-
-        #endregion
-
-        #region ToBytes(转换成字节数组)
-
-        /// <summary>
-        /// 将字符串转换成字节数组
-        /// </summary>
-        /// <param name="data">数据</param>
-        /// <returns></returns>
-        public static byte[] ToBytes(string data)
-        {
-            return ToBytes(data, Encoding.UTF8);
-        }
-
-        /// <summary>
-        /// 字符串转换成字节数组
-        /// </summary>
-        /// <param name="data">数据</param>
-        /// <param name="encoding">字符编码</param>
-        /// <returns></returns>
-        public static byte[] ToBytes(string data, Encoding encoding)
-        {
-            if (string.IsNullOrWhiteSpace(data))
-            {
-                return new byte[] { };
-            }
-
-            return encoding.GetBytes(data);
-        }
-
-        /// <summary>
-        /// 流转换成字节流
-        /// </summary>
-        /// <param name="stream">流</param>
-        /// <returns></returns>
-        public static byte[] ToBytes(Stream stream)
-        {
-            stream.Seek(0, SeekOrigin.Begin);
-            var buffer = new byte[stream.Length];
-            stream.Read(buffer, 0, buffer.Length);
-            return buffer;
-        }
-
-        #endregion
-
-        #region ToInt(转换成整数)
-
-        /// <summary>
-        /// 字节数组转换成整数
-        /// </summary>
-        /// <param name="data">数据</param>
-        /// <returns></returns>
-        public static int ToInt(byte[] data)
-        {
-            if (data == null || data.Length < 4)
-            {
-                return 0;
-            }
-
-            var buffer = new byte[4];
-            Buffer.BlockCopy(data, 0, buffer, 0, 4);
-            return BitConverter.ToInt32(buffer, 0);
         }
 
         #endregion
@@ -753,40 +397,6 @@ namespace Bing.Utils.IO
                     return result;
                 }
             }
-        }
-
-        #endregion
-
-        #region GetFileSize(获取文件大小)
-
-        /// <summary>
-        /// 获取文件大小
-        /// </summary>
-        /// <param name="filePath">文件路径</param>
-        /// <returns></returns>
-        public static FileSize GetFileSize(string filePath)
-        {
-            if (string.IsNullOrWhiteSpace(filePath))
-            {
-                throw new ArgumentNullException(nameof(filePath));
-            }
-
-            return GetFileSize(new FileInfo(filePath));
-        }
-
-        /// <summary>
-        /// 获取文件大小
-        /// </summary>
-        /// <param name="fileInfo">文件信息</param>
-        /// <returns></returns>
-        public static FileSize GetFileSize(FileInfo fileInfo)
-        {
-            if (fileInfo == null)
-            {
-                throw new ArgumentNullException(nameof(fileInfo));
-            }
-
-            return new FileSize(fileInfo.Length);
         }
 
         #endregion
