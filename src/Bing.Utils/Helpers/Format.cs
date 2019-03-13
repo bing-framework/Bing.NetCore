f93d47ab3dd7990e75f0c3bcb494dd72a8bd63dd
jian玄冰 <jianxuanhuo1@126.com>
2019-03-13 18:15:18 +0800

    新增 字符串格式化操作以及Unix时间戳操作

---
+++ src/Bing.Utils/Helpers/Format.cs
@@ -0,0 +1,55 @@
+﻿namespace Bing.Utils.Helpers
+{
+    /// <summary>
+    /// 格式化操作
+    /// </summary>
+    public static class Format
+    {
+        /// <summary>
+        /// 加密手机号码
+        /// </summary>
+        /// <param name="phone">手机号码</param>
+        /// <returns></returns>
+        public static string EncryptPhoneOfChina(string phone)
+        {
+            return string.IsNullOrWhiteSpace(phone)
+                ? string.Empty
+                : $"{phone.Substring(0, 3)}******{phone.Substring(phone.Length - 2, 2)}";
+        }
+
+        /// <summary>
+        /// 加密车牌号
+        /// </summary>
+        /// <param name="plateNumber">车牌号</param>
+        /// <returns></returns>
+        public static string EncryptPlateNumberOfChina(string plateNumber)
+        {
+            return string.IsNullOrWhiteSpace(plateNumber)
+                ? string.Empty
+                : $"{plateNumber.Substring(0, 2)}***{plateNumber.Substring(plateNumber.Length - 2, 2)}";
+        }
+
+        /// <summary>
+        /// 加密汽车VIN
+        /// </summary>
+        /// <param name="vinCode">汽车VIN</param>
+        /// <returns></returns>
+        public static string EncryptVinCode(string vinCode)
+        {
+            return string.IsNullOrWhiteSpace(vinCode)
+                ? string.Empty
+                : $"{vinCode.Substring(0, 3)}***********{vinCode.Substring(vinCode.Length - 3, 3)}";
+        }
+
+        /// <summary>
+        /// 格式化金额
+        /// </summary>
+        /// <param name="money">金额</param>
+        /// <param name="isEncrypt">是否加密。默认：false</param>
+        /// <returns></returns>
+        public static string FormatMoney(decimal money, bool isEncrypt = false)
+        {
+            return isEncrypt ? "***" : $"{money:N2}";
+        }
+    }
+}
---
+++ src/Bing.Utils/Helpers/UnixTime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Bing.Utils.Helpers
+{
+    /// <summary>
+    /// Unix时间操作
+    /// </summary>
+    public static class UnixTime
+    {
+        /// <summary>
+        /// Unix纪元时间
+        /// </summary>
+        public static DateTime EpochTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        /// <summary>
+        /// 转换为Unix时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static long ToTimestamp()
+        {
+            return ToTimestamp(DateTime.Now);
+        }
+
+        /// <summary>
+        /// 转换为Unix时间戳
+        /// </summary>
+        /// <param name="dateTime">时间</param>
+        /// <returns></returns>
+        public static long ToTimestamp(DateTime dateTime)
+        {
+            return dateTime.Kind == DateTimeKind.Utc
+                ? Convert.ToInt64((dateTime - EpochTime).TotalSeconds * 1000)
+                : Convert.ToInt64((TimeZoneInfo.ConvertTimeToUtc(dateTime) - EpochTime).TotalSeconds * 1000);
+        }
+
+        /// <summary>
+        /// 转换为DateTime对象
+        /// </summary>
+        /// <param name="timestamp">时间戳</param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(long timestamp)
+        {
+            return EpochTime.AddMilliseconds(timestamp).ToLocalTime();
+        }
+    }
+}
