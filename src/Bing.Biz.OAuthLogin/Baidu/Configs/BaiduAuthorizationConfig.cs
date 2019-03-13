4364e31c087f39fc83d11a083d73bac7859f6063
jian玄冰 <jianxuanhuo1@126.com>
2019-03-13 16:25:38 +0800

    新增计算器功能，解析字符串

--- src/Bing.Biz.OAuthLogin/Baidu/Configs/BaiduAuthorizationConfig.cs
+++ src/Bing.Biz.OAuthLogin/Baidu/Configs/BaiduAuthorizationConfig.cs
@@ -7,5 +7,13 @@ namespace Bing.Biz.OAuthLogin.Baidu.Configs
     /// </summary>
     public class BaiduAuthorizationConfig: AuthorizationConfigBase
     {
+        /// <summary>
+        /// 初始化一个<see cref="BaiduAuthorizationConfig"/>类型的实例
+        /// </summary>
+        public BaiduAuthorizationConfig()
+        {
+            AuthorizationUrl = "https://openapi.baidu.com/oauth/2.0/authorize";
+            AccessTokenUrl = "https://openapi.baidu.com/oauth/2.0/token";
+        }
     }
 }
