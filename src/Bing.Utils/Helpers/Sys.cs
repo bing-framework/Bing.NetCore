5835afbdd1105268ee584be0c21098236938752d
jian玄冰 <jianxuanhuo1@126.com>
2019-03-13 21:27:13 +0800

    新增 系统操作类

---
+++ src/Bing.Utils/Helpers/Sys.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Bing.Utils.Helpers
+{
+    /// <summary>
+    /// 系统操作
+    /// </summary>
+    public static class Sys
+    {
+        /// <summary>
+        /// 是否Linux操作系统
+        /// </summary>
+        public static bool IsLinux => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        /// <summary>
+        /// 是否Windows操作系统
+        /// </summary>
+        public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        /// <summary>
+        /// 是否苹果操作系统
+        /// </summary>
+        public static bool IsOsx => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        /// <summary>
+        /// 当前操作系统
+        /// </summary>
+        public static string System => IsWindows ? "Windows" : IsLinux ? "Linux" : IsOsx ? "OSX" : string.Empty;
+    }
+}
