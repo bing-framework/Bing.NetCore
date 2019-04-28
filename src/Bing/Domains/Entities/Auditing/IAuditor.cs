2cead3fa10ae1b0c3291b91888dcdbfd055fa6a9
jian玄冰 <jianxuanhuo1@126.com>
2019-04-28 09:59:43 +0800

    新增操作人审计 更新版本

---
+++ src/Bing/Domains/Entities/Auditing/IAuditor.cs
@@ -0,0 +1,17 @@
+﻿namespace Bing.Domains.Entities.Auditing
+{
+    /// <summary>
+    /// 操作人审计
+    /// </summary>
+    public interface IAuditor : ICreatorAudited, IModifierAudited
+    {
+    }
+
+    /// <summary>
+    /// 操作人审计
+    /// </summary>
+    /// <typeparam name="TKey">操作人标识类型</typeparam>
+    public interface IAuditor<TKey> : ICreatorAudited<TKey>, IModifierAudited<TKey>
+    {
+    }
+}
