431996c6b1a898d686e0ffed27a0059c0048652b
jian玄冰 <jianxuanhuo1@126.com>
2019-05-07 12:05:54 +0800

    重构Unix时间戳，更新版本

--- src/Bing.Utils/Helpers/UnixTime.cs
+++ src/Bing.Utils/Helpers/UnixTime.cs
@@ -15,22 +15,25 @@ namespace Bing.Utils.Helpers
         /// <summary>
         /// 转换为Unix时间戳
         /// </summary>
+        /// <param name="isContainMillisecond">是否包含毫秒</param>
         /// <returns></returns>
-        public static long ToTimestamp()
+        public static long ToTimestamp(bool isContainMillisecond = true)
         {
-            return ToTimestamp(DateTime.Now);
+            return ToTimestamp(DateTime.Now, isContainMillisecond);
         }
 
         /// <summary>
         /// 转换为Unix时间戳
         /// </summary>
         /// <param name="dateTime">时间</param>
+        /// <param name="isContainMillisecond">是否包含毫秒</param>
         /// <returns></returns>
-        public static long ToTimestamp(DateTime dateTime)
+        public static long ToTimestamp(DateTime dateTime, bool isContainMillisecond = true)
         {
             return dateTime.Kind == DateTimeKind.Utc
-                ? Convert.ToInt64((dateTime - EpochTime).TotalSeconds * 1000)
-                : Convert.ToInt64((TimeZoneInfo.ConvertTimeToUtc(dateTime) - EpochTime).TotalSeconds * 1000);
+                ? Convert.ToInt64((dateTime - EpochTime).TotalMilliseconds / (isContainMillisecond ? 1 : 1000))
+                : Convert.ToInt64((TimeZoneInfo.ConvertTimeToUtc(dateTime) - EpochTime).TotalMilliseconds /
+                                  (isContainMillisecond ? 1 : 1000));
         }
 
         /// <summary>
---
+++ tests/Bing.Utils.Tests/Helpers/UnixTimeTest.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Bing.Utils.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Bing.Utils.Tests.Helpers
+{
+    /// <summary>
+    /// Unix时间操作测试
+    /// </summary>
+    public class UnixTimeTest:TestBase
+    {
+        public UnixTimeTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Test_ToTimestamp()
+        {
+            var dto = new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds();
+            var result = UnixTime.ToTimestamp(false);
+            Output.WriteLine(dto.ToString());
+            Output.WriteLine(result.ToString());
+            Assert.Equal(dto, result);
+        }
+
+
+    }
+}
