4b897ff97bf421b1c6a5a7bca4ad7e9460e788b1
jian玄冰 <jianxuanhuo1@126.com>
2019-05-07 14:56:03 +0800

    新增缓存异步方法

--- src/Bing.EasyCaching/CacheManager.Async.cs
+++ src/Bing.EasyCaching/CacheManager.Async.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Bing.Caching;
 
-namespace Bing.Caching
+namespace Bing.EasyCaching
 {
     /// <summary>
-    /// 缓存
+    /// EasyCaching缓存管理 - 异步操作
     /// </summary>
-    public partial interface ICache
+    public partial class CacheManager: ICache
     {
         /// <summary>
         /// 是否存在指定键的缓存
         /// </summary>
         /// <param name="key">缓存键</param>
-        bool Exists(string key);
+        public async Task<bool> ExistsAsync(string key) => await _provider.ExistsAsync(key);
 
         /// <summary>
         /// 从缓存中获取数据，如果不存在，则执行获取数据操作并添加到缓存中
@@ -20,14 +22,32 @@ namespace Bing.Caching
         /// <param name="key">缓存键</param>
         /// <param name="func">获取数据操作</param>
         /// <param name="expiration">过期时间间隔</param>
-        T Get<T>(string key, Func<T> func, TimeSpan? expiration = null);
+        public async Task<T> GetAsync<T>(string key, Func<Task<T>> func, TimeSpan? expiration = null)
+        {
+            var result = await _provider.GetAsync(key, func, GetExpiration(expiration));
+            return result.Value;
+        }
+
+        /// <summary>
+        /// 从缓存中获取数据
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="type">缓存数据类型</param>
+        public async Task<object> GetAsync(string key, Type type)
+        {
+            return await _provider.GetAsync(key, type);
+        }
 
         /// <summary>
         /// 从缓存中获取数据
         /// </summary>
         /// <typeparam name="T">缓存数据类型</typeparam>
         /// <param name="key">缓存键</param>
-        T Get<T>(string key);
+        public async Task<T> GetAsync<T>(string key)
+        {
+            var result = await _provider.GetAsync<T>(key);
+            return result.Value;
+        }
 
         /// <summary>
         /// 当缓存数据不存在则添加，已存在不会添加，添加成功返回true
@@ -36,7 +56,7 @@ namespace Bing.Caching
         /// <param name="key">缓存键</param>
         /// <param name="value">值</param>
         /// <param name="expiration">过期时间间隔</param>
-        bool TryAdd<T>(string key, T value, TimeSpan? expiration = null);
+        public async Task<bool> TryAddAsync<T>(string key, T value, TimeSpan? expiration = null) => await _provider.TrySetAsync(key, value, GetExpiration(expiration));
 
         /// <summary>
         /// 添加缓存。如果已存在缓存，将覆盖
@@ -45,23 +65,24 @@ namespace Bing.Caching
         /// <param name="key">缓存键</param>
         /// <param name="value">值</param>
         /// <param name="expiration">过期时间间隔</param>
-        void Add<T>(string key, T value, TimeSpan? expiration = null);
+        public async Task AddAsync<T>(string key, T value, TimeSpan? expiration = null) => await _provider.SetAsync(key, value, GetExpiration(expiration));
 
         /// <summary>
         /// 移除缓存
         /// </summary>
         /// <param name="key">缓存键</param>
-        void Remove(string key);
+        public async Task RemoveAsync(string key) => await _provider.RemoveAsync(key);
 
         /// <summary>
         /// 通过缓存键前缀移除缓存
         /// </summary>
         /// <param name="prefix">缓存键前缀</param>
-        void RemoveByPrefix(string prefix);
+        public async Task RemoveByPrefixAsync(string prefix) => await _provider.RemoveByPrefixAsync(prefix);
 
         /// <summary>
         /// 清空缓存
         /// </summary>
-        void Clear();
+        /// <returns></returns>
+        public async Task ClearAsync() => await _provider.FlushAsync();
     }
 }
--- src/Bing/Caching/ICache.Async.cs
+++ src/Bing/Caching/ICache.Async.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Bing.Caching
 {
     /// <summary>
-    /// 缓存
+    /// 缓存 - 异步
     /// </summary>
     public partial interface ICache
     {
@@ -11,7 +12,7 @@ namespace Bing.Caching
         /// 是否存在指定键的缓存
         /// </summary>
         /// <param name="key">缓存键</param>
-        bool Exists(string key);
+        Task<bool> ExistsAsync(string key);
 
         /// <summary>
         /// 从缓存中获取数据，如果不存在，则执行获取数据操作并添加到缓存中
@@ -20,14 +21,21 @@ namespace Bing.Caching
         /// <param name="key">缓存键</param>
         /// <param name="func">获取数据操作</param>
         /// <param name="expiration">过期时间间隔</param>
-        T Get<T>(string key, Func<T> func, TimeSpan? expiration = null);
+        Task<T> GetAsync<T>(string key, Func<Task<T>> func, TimeSpan? expiration = null);
+
+        /// <summary>
+        /// 从缓存中获取数据
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="type">缓存数据类型</param>
+        Task<object> GetAsync(string key, Type type);
 
         /// <summary>
         /// 从缓存中获取数据
         /// </summary>
         /// <typeparam name="T">缓存数据类型</typeparam>
         /// <param name="key">缓存键</param>
-        T Get<T>(string key);
+        Task<T> GetAsync<T>(string key);
 
         /// <summary>
         /// 当缓存数据不存在则添加，已存在不会添加，添加成功返回true
@@ -36,7 +44,7 @@ namespace Bing.Caching
         /// <param name="key">缓存键</param>
         /// <param name="value">值</param>
         /// <param name="expiration">过期时间间隔</param>
-        bool TryAdd<T>(string key, T value, TimeSpan? expiration = null);
+        Task<bool> TryAddAsync<T>(string key, T value, TimeSpan? expiration = null);
 
         /// <summary>
         /// 添加缓存。如果已存在缓存，将覆盖
@@ -45,23 +53,24 @@ namespace Bing.Caching
         /// <param name="key">缓存键</param>
         /// <param name="value">值</param>
         /// <param name="expiration">过期时间间隔</param>
-        void Add<T>(string key, T value, TimeSpan? expiration = null);
+        Task AddAsync<T>(string key, T value, TimeSpan? expiration = null);
 
         /// <summary>
         /// 移除缓存
         /// </summary>
         /// <param name="key">缓存键</param>
-        void Remove(string key);
+        Task RemoveAsync(string key);
 
         /// <summary>
         /// 通过缓存键前缀移除缓存
         /// </summary>
         /// <param name="prefix">缓存键前缀</param>
-        void RemoveByPrefix(string prefix);
+        Task RemoveByPrefixAsync(string prefix);
 
         /// <summary>
         /// 清空缓存
         /// </summary>
-        void Clear();
+        /// <returns></returns>
+        Task ClearAsync();
     }
 }
