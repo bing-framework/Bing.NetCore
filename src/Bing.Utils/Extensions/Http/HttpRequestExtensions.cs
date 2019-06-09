54a318c058ffc1572e560705df9cace05a9fab41
jian玄冰 <jianxuanhuo1@126.com>
2019-06-10 00:03:48 +0800

    新增Http扩展方法

--- src/Bing.Utils/Extensions/Http/HttpRequestExtensions.cs
+++ src/Bing.Utils/Extensions/Http/HttpRequestExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Http;
 
 // ReSharper disable once CheckNamespace
@@ -13,7 +15,6 @@ namespace Bing.Utils.Extensions
         /// 获取Http请求的绝对路径
         /// </summary>
         /// <param name="request">Http请求</param>
-        /// <returns></returns>
         public static string GetAbsoluteUri(this HttpRequest request) => new StringBuilder()
             .Append(request.Scheme)
             .Append("://")
@@ -22,5 +23,57 @@ namespace Bing.Utils.Extensions
             .Append(request.Path)
             .Append(request.QueryString)
             .ToString();
+
+        /// <summary>
+        /// 获取查询参数
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="request">Http请求</param>
+        /// <param name="key">键</param>
+        /// <param name="defaultValue">默认值</param>
+        public static T Query<T>(this HttpRequest request, string key, T defaultValue = default)
+            where T : IConvertible
+        {
+            var value = request.Query.FirstOrDefault(x => x.Key == key);
+            if (string.IsNullOrWhiteSpace(value.Value.ToString()))
+            {
+                return defaultValue;
+            }
+
+            try
+            {
+                return (T)Convert.ChangeType(value.Value.ToString(), typeof(T));
+            }
+            catch (InvalidCastException)
+            {
+                return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// 获取表单参数
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="request">Http请求</param>
+        /// <param name="key">键</param>
+        /// <param name="defaultValue">默认值</param>
+        public static T Form<T>(this HttpRequest request, string key, T defaultValue = default)
+            where T : IConvertible
+        {
+            var value = request.Form.FirstOrDefault(x => x.Key == key);
+            if (string.IsNullOrWhiteSpace(value.Value.ToString()))
+            {
+                return defaultValue;
+            }
+
+            try
+            {
+                return (T)Convert.ChangeType(value.Value.ToString(), typeof(T));
+            }
+            catch (InvalidCastException)
+            {
+                return defaultValue;
+            }
+        }
     }
 }
