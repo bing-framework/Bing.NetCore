31912636bb08f694a17dfc64ba51ae609bcbd1e2
jian玄冰 <jianxuanhuo1@126.com>
2019-06-13 14:33:40 +0800

    新增服务以及管道

--- samples/Bing.Demo/ui/src/app/shared/bing/index.ts
+++ samples/Bing.Demo/ui/src/app/shared/bing/index.ts
@@ -3,3 +3,10 @@
 // Licensed under the MIT license
 // ================================================
 export { Bing as bing } from './bing';
+export { HttpContentType, HttpMethod } from './core/http-helper';
+export { IDialogOptions } from './common/dialog';
+export { IKey, ViewModel, QueryParameter } from './models/model';
+export { TreeViewModel, TreeQueryParameter } from './models/tree-model';
+export { PagerList } from './models/pager-list';
+export { Result, FailResult, StateCode } from './models/result';
+export { SelectItem } from './models/select-model';
