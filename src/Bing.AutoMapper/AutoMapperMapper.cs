ba5ec5528bde82f20b48da63dc0f6809753491b4
jian玄冰 <jianxuanhuo1@126.com>
2019-08-14 23:01:35 +0800

    fix: 修复新版本AutoMapper 无法动态映射问题

--- src/Bing.AutoMapper/AutoMapperMapper.cs
+++ src/Bing.AutoMapper/AutoMapperMapper.cs
@@ -6,15 +6,15 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using AutoMapper;
+using AutoMapper.Configuration;
 using AutoMapper.QueryableExtensions;
-using IMapper = Bing.Mapping.IMapper;
 
 namespace Bing.AutoMapper
 {
     /// <summary>
     /// AutoMapper映射类
     /// </summary>
-    public class AutoMapperMapper : IMapper
+    public class AutoMapperMapper : Bing.Mapping.IMapper
     {
         /// <summary>
         /// 同步锁
@@ -28,10 +28,7 @@ namespace Bing.AutoMapper
         /// </summary>
         /// <typeparam name="TDestination">目标类型</typeparam>
         /// <param name="source">源对象</param>
-        public TDestination MapTo<TDestination>(object source) where TDestination : new()
-        {
-            return MapTo(source, new TDestination());
-        }
+        public TDestination MapTo<TDestination>(object source) where TDestination : new() => MapTo(source, new TDestination());
 
         /// <summary>
         /// 将源对象映射到目标对象
@@ -40,10 +37,7 @@ namespace Bing.AutoMapper
         /// <typeparam name="TDestination">目标类型</typeparam>
         /// <param name="source">源对象</param>
         /// <param name="destination">目标对象</param>
-        public TDestination MapTo<TSource, TDestination>(TSource source, TDestination destination)
-        {
-            return MapTo<TDestination>(source, destination);
-        }
+        public TDestination MapTo<TSource, TDestination>(TSource source, TDestination destination) => MapTo<TDestination>(source, destination);
 
         /// <summary>
         /// 将源对象映射到目标对象
@@ -54,27 +48,19 @@ namespace Bing.AutoMapper
         private static TDestination MapTo<TDestination>(object source, TDestination destination)
         {
             if (source == null)
-            {
-                return default(TDestination);
-            }
+                return default;
             if (destination == null)
-            {
-                return default(TDestination);
-            }
+                return default;
             var sourceType = GetType(source);
             var destinationType = GetType(destination);
             var map = GetMap(sourceType, destinationType);
             if (map != null)
-            {
                 return Mapper.Map(source, destination);
-            }
             lock (Sync)
             {
                 map = GetMap(sourceType, destinationType);
                 if (map != null)
-                {
                     return Mapper.Map(source, destination);
-                }
                 InitMaps(sourceType, destinationType);
             }
             return Mapper.Map(source, destination);
@@ -116,18 +102,12 @@ namespace Bing.AutoMapper
         {
             var type = obj.GetType();
             if (obj is System.Collections.IEnumerable == false)
-            {
                 return type;
-            }
             if (type.IsArray)
-            {
                 return type.GetElementType();
-            }
             var genericArgumentsTypes = type.GetTypeInfo().GetGenericArguments();
             if (genericArgumentsTypes == null || genericArgumentsTypes.Length == 0)
-            {
                 throw new ArgumentException("泛型类型参数不能为空");
-            }
             return genericArgumentsTypes[0];
         }
 
@@ -141,8 +121,8 @@ namespace Bing.AutoMapper
             try
             {
                 var maps = Mapper.Configuration.GetAllTypeMaps();
-                ClearConfig();
-                Mapper.Initialize(config => { config.CreateMap(sourceType, destinationType); });
+                Mapper.Reset();
+                InitMapper(sourceType, destinationType);
                 foreach (var map in maps)
                 {
                     Mapper.Configuration.RegisterTypeMap(map);
@@ -150,18 +130,20 @@ namespace Bing.AutoMapper
             }
             catch (InvalidOperationException)
             {
-                Mapper.Initialize(config => { config.CreateMap(sourceType, destinationType); });
+                InitMapper(sourceType,destinationType);
             }
         }
 
         /// <summary>
-        /// 清空配置
+        /// 初始化映射器
         /// </summary>
-        private static void ClearConfig()
+        /// <param name="sourceType">源类型</param>
+        /// <param name="destinationType">目标类型</param>
+        private static void InitMapper(Type sourceType, Type destinationType)
         {
-            var typeMapper = typeof(Mapper).GetTypeInfo();
-            var configuration = typeMapper.GetDeclaredField("_configuration");
-            configuration.SetValue(null, null, BindingFlags.Static, null, CultureInfo.CurrentCulture);
+            var config = new MapperConfigurationExpression();
+            config.CreateMap(sourceType, destinationType);
+            Mapper.Initialize(config);
         }
 
         #endregion
@@ -188,7 +170,6 @@ namespace Bing.AutoMapper
         /// <typeparam name="TOutputDto">输出Dto类型</typeparam>
         /// <param name="source">源类型</param>
         /// <param name="membersToExpand">成员展开</param>
-        /// <returns></returns>
         public IQueryable<TOutputDto> ToOutput<TOutputDto>(IQueryable source, params Expression<Func<TOutputDto, object>>[] membersToExpand)
         {
             return source.ProjectTo(membersToExpand);
