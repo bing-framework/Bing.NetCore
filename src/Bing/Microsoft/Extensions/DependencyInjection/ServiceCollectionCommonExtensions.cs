c89bdfc54f4b7dc890501188e7de22a0423fcbf8
jian玄冰 <jianxuanhuo1@126.com>
2019-11-13 00:09:34 +0800

    fix: 修改位置

--- src/Bing/Microsoft/Extensions/DependencyInjection/ServiceCollectionCommonExtensions.cs
+++ src/Bing/Microsoft/Extensions/DependencyInjection/ServiceCollectionCommonExtensions.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using Bing.Reflections;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace Bing.Dependency
+namespace Microsoft.Extensions.DependencyInjection
 {
     /// <summary>
-    /// 服务集合(<see cref="IServiceCollection"/>) 扩展
+    /// 服务集合 - 通用 扩展
     /// </summary>
-    public static class ServiceCollectionExtensions
+    public static class ServiceCollectionCommonExtensions
     {
+        /// <summary>
+        /// 是否已注册指定类型
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="services">服务集合</param>
+        public static bool IsAdded<T>(this IServiceCollection services) => services.IsAdded(typeof(T));
+
+        /// <summary>
+        /// 是否已注册指定类型
+        /// </summary>
+        /// <param name="services">服务集合</param>
+        /// <param name="type">类型</param>
+        public static bool IsAdded(this IServiceCollection services, Type type) => services.Any(x => x.ServiceType == type);
+
+        /// <summary>
+        /// 获取单例注册服务对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="services">服务集合</param>
+        public static T GetSingletonInstanceOrNull<T>(this IServiceCollection services) => (T)services.FirstOrDefault(x => x.ServiceType == typeof(T))?.ImplementationInstance;
+
+        /// <summary>
+        /// 获取单例注册服务对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="services">服务集合</param>
+        public static T GetSingletonInstance<T>(this IServiceCollection services)
+        {
+            var instance = services.GetSingletonInstanceOrNull<T>();
+            if (instance == null)
+                throw new InvalidOperationException($"无法找到已注册的单例服务: {typeof(T).AssemblyQualifiedName}");
+            return instance;
+        }
+
         /// <summary>
         /// 如果指定服务不存在，则添加指定服务
         /// </summary>
@@ -59,39 +92,6 @@ namespace Bing.Dependency
                 services.AddSingleton<TServiceType>(item);
             }
             return item;
-        }
-
-        /// <summary>
-        /// 获取单例注册服务对象
-        /// </summary>
-        /// <typeparam name="T">对象类型</typeparam>
-        /// <param name="services">服务集合</param>
-        public static T GetSingletonInstanceOrNull<T>(this IServiceCollection services) => (T)services.FirstOrDefault(d => d.ServiceType == typeof(T))?.ImplementationInstance;
-
-        /// <summary>
-        /// 获取单例注册服务对象
-        /// </summary>
-        /// <typeparam name="T">对象类型</typeparam>
-        /// <param name="services">服务集合</param>
-        public static T GetSingletonInstance<T>(this IServiceCollection services)
-        {
-            var instance = services.GetSingletonInstanceOrNull<T>();
-            if (instance == null)
-                throw new InvalidOperationException($"无法找到已注册的单例服务: {typeof(T).AssemblyQualifiedName}");
-            return instance;
-        }
-
-        /// <summary>
-        /// 从Scoped字典获取指定类型的值
-        /// </summary>
-        /// <typeparam name="T">类型</typeparam>
-        /// <param name="dictionary">字典</param>
-        /// <param name="key">键名</param>
-        public static T GetValue<T>(this ScopedDictionary dictionary, string key) where T : class
-        {
-            if (dictionary.TryGetValue(key, out object obj))
-                return obj as T;
-            return default;
         }
 
         /// <summary>
