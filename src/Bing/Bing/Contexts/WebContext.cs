959acd7a14f33a69e7d635097da6f33d32cae7ce
jian玄冰 <jianxuanhuo1@126.com>
2019-11-15 10:08:52 +0800

    chore: 移动 Contexts、Datas

---
+++ src/Bing/Bing/Contexts/WebContext.cs
@@ -0,0 +1,46 @@
+﻿using Bing.Utils.Helpers;
+
+namespace Bing.Contexts
+{
+    /// <summary>
+    /// Web上下文
+    /// </summary>
+    public class WebContext : IContext
+    {
+        /// <summary>
+        /// 跟踪号
+        /// </summary>
+        public string TraceId => Web.HttpContext?.TraceIdentifier;
+
+        /// <summary>
+        /// 添加对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="key">键名</param>
+        /// <param name="value">对象</param>
+        public void Add<T>(string key, T value)
+        {
+            if (Web.HttpContext == null)
+                return;
+            Web.HttpContext.Items[key] = value;
+        }
+
+        /// <summary>
+        /// 获取对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="key">键名</param>
+        public T Get<T>(string key)
+        {
+            if (Web.HttpContext == null)
+                return default;
+            return Conv.To<T>(Web.HttpContext.Items[key]);
+        }
+
+        /// <summary>
+        /// 移除对象
+        /// </summary>
+        /// <param name="key">键名</param>
+        public void Remove(string key) => Web.HttpContext?.Items.Remove(key);
+    }
+}
---
+++ src/Bing/Bing/Datas/Stores/Operations/IFindAllNoTracking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Bing.Domains.Entities;
+
+namespace Bing.Datas.Stores.Operations
+{
+    /// <summary>
+    /// 查找实体列表
+    /// </summary>
+    /// <typeparam name="TEntity">对象类型</typeparam>
+    /// <typeparam name="TKey">对象标识类型</typeparam>
+    public interface IFindAllNoTracking<TEntity, in TKey> where TEntity : class, IKey<TKey>
+    {
+        /// <summary>
+        /// 查找实体列表，不跟踪
+        /// </summary>
+        /// <param name="predicate">查询条件</param>
+        List<TEntity> FindAllNoTracking(Expression<Func<TEntity, bool>> predicate = null);
+    }
+}
