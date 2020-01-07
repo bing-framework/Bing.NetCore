b2638128deccf57990f23556f890e5cc851b6653
jian玄冰 <jianxuanhuo1@126.com>
2020-01-07 21:43:27 +0800

    style: 格式化代码

--- src/Bing.Utils/Expressions/ParameterRebinder.cs
+++ src/Bing.Utils/Expressions/ParameterRebinder.cs
@@ -17,35 +17,25 @@ namespace Bing.Utils.Expressions
         /// 初始化一个<see cref="ParameterRebinder"/>类型的实例
         /// </summary>
         /// <param name="map">参数字典</param>
-        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
-        {
-            _map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
-        }
+        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map) => _map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
 
         /// <summary>
         /// 替换参数
         /// </summary>
         /// <param name="map">参数字典</param>
         /// <param name="exp">表达式</param>
-        /// <returns></returns>
         public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map,
-            Expression exp)
-        {
-            return new ParameterRebinder(map).Visit(exp);
-        }
+            Expression exp) =>
+            new ParameterRebinder(map).Visit(exp);
 
         /// <summary>
         /// 访问参数
         /// </summary>
         /// <param name="parameterExpression">参数表达式</param>
-        /// <returns></returns>
         protected override Expression VisitParameter(ParameterExpression parameterExpression)
         {
-            ParameterExpression replacement;
-            if (_map.TryGetValue(parameterExpression, out replacement))
-            {
+            if (_map.TryGetValue(parameterExpression, out var replacement))
                 parameterExpression = replacement;
-            }
             return base.VisitParameter(parameterExpression);
         }
     }
