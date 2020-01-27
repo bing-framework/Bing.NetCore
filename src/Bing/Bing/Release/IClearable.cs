b3874f8685010b2ac316e32c711d3fa660e559a0
jian玄冰 <jianxuanhuo1@126.com>
2020-01-27 15:45:11 +0800

    fix: 加入缺失文件

---
+++ src/Bing/Bing/Release/IClearable.cs
@@ -0,0 +1,13 @@
+﻿namespace Bing.Release
+{
+    /// <summary>
+    /// 可清空的接口
+    /// </summary>
+    public interface IClearable
+    {
+        /// <summary>
+        /// 清空
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ src/Bing/Bing/Release/ICloseable.cs
@@ -0,0 +1,20 @@
+﻿using Bing.Core.Data;
+
+namespace Bing.Release
+{
+    /// <summary>
+    /// 可关闭的接口
+    /// </summary>
+    public interface ICloseable
+    {
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// 关闭后事件
+        /// </summary>
+        event DataHandler Closed;
+    }
+}
