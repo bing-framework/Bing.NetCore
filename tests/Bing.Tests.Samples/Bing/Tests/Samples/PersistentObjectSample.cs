a08d902f2a00c2d0bbed2ac33ff3799e2bfb8b2e
jian玄冰 <jianxuanhuo1@126.com>
2020-02-01 16:09:42 +0800

    test: 新增 测试例子

---
+++ tests/Bing.Tests.Samples/Bing/Tests/Samples/PersistentObjectSample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Bing.Datas.Persistence;
+
+namespace Bing.Tests.Samples
+{
+    /// <summary>
+    /// 持久化对象测试样例
+    /// </summary>
+    public class PersistentObjectSample : PersistentEntityBase<Guid>
+    {
+        /// <summary>
+        /// 初始化一个<see cref="PersistentObjectSample"/>类型的实例
+        /// </summary>
+        public PersistentObjectSample() : this(Guid.Empty) { }
+
+        /// <summary>
+        /// 初始化一个<see cref="PersistentObjectSample"/>类型的实例
+        /// </summary>
+        public PersistentObjectSample(Guid id) => Id = id;
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 编码
+        /// </summary>
+        public string Code { get; set; }
+    }
+
+    /// <summary>
+    /// 持久化对象测试样例2
+    /// </summary>
+    public class PersistentObjectSample2 : PersistentObjectBase<Guid>
+    {
+        /// <summary>
+        /// 初始化一个<see cref="PersistentObjectSample2"/>类型的实例
+        /// </summary>
+        public PersistentObjectSample2() : this(Guid.Empty) { }
+
+        /// <summary>
+        /// 初始化一个<see cref="PersistentObjectSample2"/>类型的实例
+        /// </summary>
+        public PersistentObjectSample2(Guid id) => Id = id;
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 编码
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
---
+++ tests/Bing.Tests.Samples/Bing/Tests/Samples/Role.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Bing.Domains.Entities.Trees;
+using Bing.Domains.Repositories;
+
+namespace Bing.Tests.Samples
+{
+    /// <summary>
+    /// 角色
+    /// </summary>
+    public class Role : TreeEntityBase<Role, Guid, Guid?>
+    {
+        /// <summary>
+        /// 初始化一个<see cref="Role"/>类型的实例
+        /// </summary>
+        public Role() : this(Guid.Empty, "", 0)
+        {
+        }
+
+        /// <summary>
+        /// 初始化一个<see cref="Role"/>类型的实例
+        /// </summary>
+        /// <param name="id">标识</param>
+        /// <param name="path">路径</param>
+        /// <param name="level">级数</param>
+        public Role(Guid id, string path, int level) : base(id, path, level)
+        {
+        }
+    }
+
+    /// <summary>
+    /// 角色仓储
+    /// </summary>
+    public interface IRoleRepository : ITreeRepository<Role> { }
+}
