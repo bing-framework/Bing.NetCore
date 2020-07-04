aca222a330b19eaff9580317e829293f7aafbce6
jian玄冰 <jianxuanhuo1@126.com>
2020-07-04 17:19:42 +0800

    feat: 新增 字符串 Base64 扩展

--- src/Bing.Utils/Bing/Text/Extensions/String/Extensions.String.From.cs
+++ src/Bing.Utils/Bing/Text/Extensions/String/Extensions.String.From.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
+using Bing.Conversions;
 
 // ReSharper disable once CheckNamespace
 namespace Bing.Text
@@ -10,5 +9,19 @@ namespace Bing.Text
     /// </summary>
     public static partial class StringExtensions
     {
+        /// <summary>
+        /// 将Base64字符串转换为字节数组
+        /// </summary>
+        /// <param name="base64String">Base64字符串</param>
+        public static byte[] FromBase64StringToBytes(this string base64String) =>
+            Base64Converter.FromBase64StringToBytes(base64String);
+
+        /// <summary>
+        /// 将Base64字符串转换为字符串
+        /// </summary>
+        /// <param name="base64String">Base64字符串</param>
+        /// <param name="encoding">字符编码</param>
+        public static string FromBase64String(this string base64String, Encoding encoding = null) =>
+            Base64Converter.FromBase64String(base64String, encoding);
     }
 }
