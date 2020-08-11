714b88f20c6bd6a1735982d99a00ba8c72be2dbd
jian玄冰 <jianxuanhuo1@126.com>
2020-08-11 16:07:58 +0800

    feat: 新增 字典扩展-AddOrUpdate

--- utils/src/Bing.Utils/Bing/Extensions/Collections/DictionaryExtensions.cs
+++ utils/src/Bing.Utils/Bing/Extensions/Collections/DictionaryExtensions.cs
@@ -315,5 +315,62 @@ namespace Bing.Extensions
         }
 
         #endregion
+
+        #region AddOrUpdate(添加或更新)
+
+        /// <summary>
+        /// 添加或更新
+        /// </summary>
+        /// <typeparam name="TKey">键类型</typeparam>
+        /// <typeparam name="TValue">值类型</typeparam>
+        /// <param name="this">字典</param>
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        public static TValue AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue value)
+        {
+            if (!@this.ContainsKey(key))
+                @this.Add(new KeyValuePair<TKey, TValue>(key, value));
+            else
+                @this[key] = value;
+            return @this[key];
+        }
+
+        /// <summary>
+        /// 添加或更新
+        /// </summary>
+        /// <typeparam name="TKey">键类型</typeparam>
+        /// <typeparam name="TValue">值类型</typeparam>
+        /// <param name="this">字典</param>
+        /// <param name="key">键</param>
+        /// <param name="addValue">添加值</param>
+        /// <param name="updateValueFactory">更新值函数</param>
+        public static TValue AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue addValue, Func<TKey, TValue, TValue> updateValueFactory)
+        {
+            if (!@this.ContainsKey(key))
+                @this.Add(new KeyValuePair<TKey, TValue>(key, addValue));
+            else
+                @this[key] = updateValueFactory(key, @this[key]);
+            return @this[key];
+        }
+
+        /// <summary>
+        /// 添加或更新
+        /// </summary>
+        /// <typeparam name="TKey">键类型</typeparam>
+        /// <typeparam name="TValue">值类型</typeparam>
+        /// <param name="this">字典</param>
+        /// <param name="key">键</param>
+        /// <param name="addValueFactory">添加值函数</param>
+        /// <param name="updateValueFactory">更新值函数</param>
+        public static TValue AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, Func<TKey, TValue> addValueFactory, Func<TKey, TValue, TValue> updateValueFactory)
+        {
+            if (!@this.ContainsKey(key))
+                @this.Add(new KeyValuePair<TKey, TValue>(key, addValueFactory(key)));
+            else
+                @this[key] = updateValueFactory(key, @this[key]);
+            return @this[key];
+        }
+
+        #endregion
     }
 }
