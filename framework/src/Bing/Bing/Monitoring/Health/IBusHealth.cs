36067d2798b37a0d19381a20f703f9956c424fa5
jian玄冰 <jianxuanhuo1@126.com>
2020-08-17 11:50:27 +0800

    tmp: 增加 健康检查 定义

---
+++ framework/src/Bing/Bing/Monitoring/Health/IBusHealth.cs
@@ -0,0 +1,15 @@
+﻿namespace Bing.Monitoring.Health
+{
+    /// <summary>
+    /// 定义业务健康检查
+    /// </summary>
+    public interface IBusHealth
+    {
+        /// <summary>
+        /// 业务名称
+        /// </summary>
+        string Name { get; }
+
+
+    }
+}
