f47dfa31fafd3c9dd97f1f8659d604f9fa5a2d60
jian玄冰 <jianxuanhuo1@126.com>
2020-08-21 16:07:44 +0800

    feat: 新增应用服务实现类

---
+++ framework/src/Bing.Ddd.Application/Bing/Application/Services/ApplicationService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Bing.DependencyInjection;
+using Bing.Logs;
+using Bing.Sessions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bing.Application.Services
+{
+    /// <summary>
+    /// 应用服务
+    /// </summary>
+    public abstract class ApplicationService : IApplicationService
+    {
+        /// <summary>
+        /// 日志
+        /// </summary>
+        private ILog _log;
+
+        /// <summary>
+        /// 服务提供程序锁
+        /// </summary>
+        protected readonly object ServiceProviderLock = new object();
+
+        /// <summary>
+        /// 日志
+        /// </summary>
+        public ILog Log => _log ??= GetLog();
+
+        /// <summary>
+        /// 用户会话
+        /// </summary>
+        public virtual ISession Session => Bing.Sessions.Session.Instance;
+
+        /// <summary>
+        /// 服务定位器
+        /// </summary>
+        public virtual IServiceProvider ServiceProvider => ServiceLocator.Instance.ScopedProvider;
+
+        /// <summary>
+        /// 懒加载获取请求服务
+        /// </summary>
+        /// <typeparam name="TService">服务类型</typeparam>
+        /// <param name="reference">服务引用</param>
+        protected TService LazyGetRequiredService<TService>(ref TService reference)
+        {
+            if (reference == null)
+            {
+                lock (ServiceProviderLock)
+                {
+                    if (reference == null)
+                        reference = ServiceProvider.GetRequiredService<TService>();
+                }
+            }
+            return reference;
+        }
+
+        /// <summary>
+        /// 获取日志操作
+        /// </summary>
+        protected virtual ILog GetLog()
+        {
+            try
+            {
+                return Bing.Logs.Log.GetLog(this);
+            }
+            catch
+            {
+                return Bing.Logs.Log.Null;
+            }
+        }
+    }
+}
