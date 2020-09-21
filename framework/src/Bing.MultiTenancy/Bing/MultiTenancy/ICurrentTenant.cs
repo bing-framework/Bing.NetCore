788f20f443e6144e471e394f2f62f219304ac3c9
jian玄冰 <jianxuanhuo1@126.com>
2020-09-21 23:32:30 +0800

    feat: 新增 多租户类库

---
+++ framework/src/Bing.MultiTenancy/Bing/MultiTenancy/ICurrentTenant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Bing.MultiTenancy
+{
+    /// <summary>
+    /// 当前租户
+    /// </summary>
+    public interface ICurrentTenant
+    {
+        /// <summary>
+        /// 是否可用的
+        /// </summary>
+        bool IsAvailable { get; }
+
+        /// <summary>
+        /// 租户标识
+        /// </summary>
+        string TenantId { get; }
+
+        /// <summary>
+        /// 租户名称
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// 变更
+        /// </summary>
+        /// <param name="tenantId">租户标识</param>
+        /// <param name="name">租户名称</param>
+        IDisposable Change(string tenantId, string name = null);
+    }
+}
---
+++ framework/src/Bing.MultiTenancy/Bing/MultiTenancy/IMultiTenant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bing.MultiTenancy
+{
+    /// <summary>
+    /// 多租户
+    /// </summary>
+    public interface IMultiTenant : IMultiTenant<Guid?>
+    {
+    }
+
+    /// <summary>
+    /// 多租户
+    /// </summary>
+    /// <typeparam name="TKey">标识类型</typeparam>
+    public interface IMultiTenant<TKey>
+    {
+        /// <summary>
+        /// 租户标识
+        /// </summary>
+        TKey TenantId { get; set; }
+    }
+}
