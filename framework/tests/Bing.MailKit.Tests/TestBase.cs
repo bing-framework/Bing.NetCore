648614da9f7d755ee7cd6bcba628ae2aac86d15a
jian玄冰 <jianxuanhuo1@126.com>
2020-09-28 11:39:34 +0800

    feat: 新增 MailKit 单元测试

---
+++ framework/tests/Bing.MailKit.Tests/TestBase.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Bing.MailKit.Tests
+{
+    public class TestBase
+    {
+        protected ITestOutputHelper Output;
+
+        public TestBase(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+    }
+}
