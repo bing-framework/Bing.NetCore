0f5efb724a446440fae2fa8531013cd78cc8d8d3
jian玄冰 <jianxuanhuo1@126.com>
2020-11-20 14:50:46 +0800

    fix: 增加 Data 抽象接口

---
+++ framework/src/Bing.Abstractions/Bing/Data/Common/IDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Bing.Data.Common
+{
+    /// <summary>
+    /// DbContext 元接口
+    /// </summary>
+    public interface IDbContext
+    {
+    }
+}
---
+++ framework/src/Bing.Abstractions/Bing/Data/Common/IDbSet.cs
@@ -0,0 +1,10 @@
+﻿namespace Bing.Data.Common
+{
+    /// <summary>
+    /// DbSet 元接口
+    /// </summary>
+    /// <typeparam name="TEntity">实体类型</typeparam>
+    public interface IDbSet<TEntity> where TEntity : class
+    {
+    }
+}
