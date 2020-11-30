fa375ff4fe2574f5b28ec9df0d2ded33a7f7751c
jian玄冰 <jianxuanhuo1@126.com>
2020-11-30 14:34:39 +0800

    fix: 优化 Options 自动注入

--- framework/src/Bing.AspNetCore/Bing/AspNetCore/Extensions/Extensions.ServiceCollection.cs
+++ framework/src/Bing.AspNetCore/Bing/AspNetCore/Extensions/Extensions.ServiceCollection.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using Bing.AspNetCore.Mvc;
+﻿using Bing.AspNetCore.Mvc;
 using Bing.AspNetCore.Uploads;
-using Bing.Configuration;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -16,42 +11,6 @@ namespace Bing.AspNetCore.Extensions
     /// </summary>
     public static class BingServiceCollectionExtensions
     {
-        /// <summary>
-        /// 注册配置类型
-        /// </summary>
-        /// <param name="services">服务集合</param>
-        /// <param name="assembly">程序集</param>
-        public static IServiceCollection AddConfigType(this IServiceCollection services, Assembly assembly)
-        {
-            var typeInfos = assembly.GetTypes().Where(type => type.GetCustomAttributes<ConfigTypeAttribute>().Any())
-                .Select(x => new {Type = x, Attribute = x.GetCustomAttributes<ConfigTypeAttribute>().FirstOrDefault()})
-                .ToArray();
-            foreach (var typeInfo in typeInfos)
-            {
-                var type = typeInfo.Type;
-                var attribute = typeInfo.Attribute;
-                services.TryAdd(new ServiceDescriptor(type, provider =>
-                {
-                    var configuration = provider.GetService<IConfiguration>();
-                    var constructor = type.GetConstructor(new[] {typeof(IConfiguration)});
-                    var result = constructor == null
-                        ? Activator.CreateInstance(type)
-                        : Activator.CreateInstance(type, configuration);
-                    if (string.IsNullOrWhiteSpace(attribute.SectionName))
-                    {
-                        configuration.Bind(result);
-                    }
-                    else
-                    {
-                        var section = provider.GetService<IConfiguration>().GetSection(attribute.SectionName);
-                        section.Bind(result);
-                    }
-                    return result;
-                }, attribute.ReloadOnChange ? ServiceLifetime.Transient : ServiceLifetime.Singleton));
-            }
-            return services;
-        }
-
         /// <summary>
         /// 注册上传服务
         /// </summary>
--- framework/src/Bing/Bing/Configuration/OptionsTypeAttribute.cs
+++ framework/src/Bing/Bing/Configuration/OptionsTypeAttribute.cs
@@ -6,7 +6,7 @@ namespace Bing.Configuration
     /// 配置类型 特性
     /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
-    public class ConfigTypeAttribute : Attribute
+    public class OptionsTypeAttribute : Attribute
     {
         /// <summary>
         /// 配置节点名称。默认为：类名称
@@ -14,19 +14,12 @@ namespace Bing.Configuration
         public string SectionName { get; }
 
         /// <summary>
-        /// 是否允许重载
-        /// </summary>
-        public bool ReloadOnChange { get; }
-
-        /// <summary>
-        /// 初始化一个<see cref="ConfigTypeAttribute"/>类型的实例
+        /// 初始化一个<see cref="OptionsTypeAttribute"/>类型的实例
         /// </summary>
         /// <param name="sectionName">配置节点名称</param>
-        /// <param name="reloadOnChange">是否允许重载</param>
-        public ConfigTypeAttribute(string sectionName = null, bool reloadOnChange = false)
+        public OptionsTypeAttribute(string sectionName = null)
         {
             SectionName = sectionName;
-            ReloadOnChange = reloadOnChange;
         }
     }
 }
