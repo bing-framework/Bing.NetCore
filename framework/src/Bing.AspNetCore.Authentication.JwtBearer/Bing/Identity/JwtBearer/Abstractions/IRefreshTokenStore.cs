2afc0cbbb5fea762c74f7697ce16738dbf32f10d
jian玄冰 <jianxuanhuo1@126.com>
2021-09-09 16:46:43 +0800

    feat: 新增JwtBearer库

---
+++ framework/src/Bing.AspNetCore.Authentication.JwtBearer/Bing/Identity/JwtBearer/Abstractions/IRefreshTokenStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bing.Identity.JwtBearer.Abstractions
+{
+    /// <summary>
+    /// Jwy刷新令牌存储器
+    /// </summary>
+    public interface IRefreshTokenStore
+    {
+    }
+}
