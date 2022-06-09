ff3af1be29322f260e10b71648663ed3106648b5
jian玄冰 <jianxuanhuo1@126.com>
2022-06-09 22:29:36 +0800

    style: 移除日志字段

--- framework/src/Bing.Logging/Bing/Logging/Log.cs
+++ framework/src/Bing.Logging/Bing/Logging/Log.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -402,7 +401,5 @@ namespace Bing.Logging
         }
 
         #endregion
-
-
     }
 }
