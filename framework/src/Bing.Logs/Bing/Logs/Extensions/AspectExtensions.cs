84e6d4f4f849143c13d5fd914aa0736d41da7f26
jian玄冰 <jianxuanhuo1@126.com>
2023-01-21 11:31:12 +0800

    style: remove logs

--- framework/src/Bing.Logs/Bing/Logs/Extensions/AspectExtensions.cs
+++ framework/src/Bing.Logs/Bing/Logs/Extensions/AspectExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using AspectCore.DynamicProxy.Parameters;
+﻿using AspectCore.DynamicProxy.Parameters;
 using Bing.Extensions;
 
 // ReSharper disable once CheckNamespace
