0371b832feb35347696f11d5ea15d19efc79b95c
jian玄冰 <jianxuanhuo1@126.com>
2023-02-10 17:41:16 +0800

    tmp: 新增 模块 信息

---
+++ framework/src/Bing.Security/Bing/Authorization/Modules/ModuleInfo.cs
@@ -0,0 +1,27 @@
+﻿namespace Bing.Authorization.Modules;
+
+/// <summary>
+/// 模块信息
+/// </summary>
+public class ModuleInfo
+{
+    /// <summary>
+    /// 代码
+    /// </summary>
+    public string Code { get; set; }
+
+    /// <summary>
+    /// 名称
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 排序号
+    /// </summary>
+    public int SortId { get; set; }
+
+    /// <summary>
+    /// 位置。父模块Code以点号 . 相连的字符串
+    /// </summary>
+    public string Position { get; set; }
+}
