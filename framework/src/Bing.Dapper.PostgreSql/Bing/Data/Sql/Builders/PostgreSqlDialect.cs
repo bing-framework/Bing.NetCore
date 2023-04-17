ee8e21f0ea36d39b68b3f4ec8bb7494deef317aa
jian玄冰 <jianxuanhuo1@126.com>
2023-04-17 11:33:42 +0800

    feat: 新增 Bing.Dapper.SqlServer、Bing.Dapper.PostgreSql 组件

---
+++ framework/src/Bing.Dapper.PostgreSql/Bing/Data/Sql/Builders/PostgreSqlDialect.cs
@@ -0,0 +1,25 @@
+﻿using Bing.Data.Sql.Builders.Core;
+
+namespace Bing.Data.Sql.Builders;
+
+/// <summary>
+/// PostgreSql方言
+/// </summary>
+public class PostgreSqlDialect : DialectBase
+{
+    /// <summary>
+    /// 封闭构造函数
+    /// </summary>
+    private PostgreSqlDialect() { }
+
+    /// <summary>
+    /// PostgreSql方言实例
+    /// </summary>
+    public static IDialect Instance => new PostgreSqlDialect();
+
+    /// <inheritdoc />
+    public override char OpeningIdentifier => '"';
+
+    /// <inheritdoc />
+    public override char ClosingIdentifier => '"';
+}
---
+++ framework/src/Bing.Dapper.SqlServer/Bing/Data/Sql/SqlServerDatabaseFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Bing.Data.Sql;
+
+/// <summary>
+/// Sql Server数据库工厂
+/// </summary>
+public class SqlServerDatabaseFactory : IDatabaseFactory
+{
+    /// <inheritdoc />
+    public IDatabase Create(string connection) => new DefaultDatabase(new SqlConnection(connection));
+}
