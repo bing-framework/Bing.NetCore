876188cf7947ba352005a0eadf4e6ac1616284f9
jian玄冰 <jianxuanhuo1@126.com>
2023-06-12 14:58:11 +0800

    test: 新增 sample 模块，用于测试项目内存占用

---
+++ samples/Bing.Samples.WebApi/Modules/AppModule.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using Bing.AspNetCore;
+using Bing.Core.Modularity;
+
+namespace Bing.Samples.WebApi.Modules;
+
+/// <summary>
+/// 应用程序模块
+/// </summary>
+[Description("应用程序模块")]
+[DependsOnModule(typeof(AspNetCoreModule))]
+public class AppModule : AspNetCoreBingModule
+{
+    /// <summary>
+    /// 模块级别。级别越小越先启动
+    /// </summary>
+    public override ModuleLevel Level => ModuleLevel.Application;
+
+    /// <summary>
+    /// 添加服务。将模块服务添加到依赖注入服务容器中
+    /// </summary>
+    /// <param name="services">服务集合</param>
+    public override IServiceCollection AddServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        return services;
+    }
+
+    /// <summary>
+    /// 应用AspNetCore的服务业务
+    /// </summary>
+    /// <param name="app">应用程序构建器</param>
+    public override void UseModule(IApplicationBuilder app)
+    {
+        app.UseRouting();
+    }
+}
