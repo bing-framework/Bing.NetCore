28e77caf420ed808844cd142ae942b195c159498
jian玄冰 <jianxuanhuo1@126.com>
2024-03-18 11:39:35 +0800

    ✨ feat(Middleware): 增加 中间件基类，用于禁用部分功能

---
+++ framework/src/Bing.AspNetCore/Bing/AspNetCore/Middleware/BingMiddlewareBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace Bing.AspNetCore.Middleware;
+
+/// <summary>
+/// 中间件基类
+/// </summary>
+public abstract class BingMiddlewareBase : IMiddleware
+{
+    /// <summary>
+    /// 判断是否应该跳过某些指定的处理逻辑。
+    /// </summary>
+    /// <param name="context">当前HTTP请求的上下文</param>
+    /// <param name="next">下一个请求委托</param>
+    protected virtual Task<bool> ShouldSkipAsync(HttpContext context, RequestDelegate next)
+    {
+        var endpoint = context.GetEndpoint();
+        var controllerActionDescriptor = endpoint?.Metadata.GetMetadata<ControllerActionDescriptor>();
+        var disableBingFeaturesAttribute = controllerActionDescriptor?.ControllerTypeInfo.GetCustomAttribute<DisableBingFeaturesAttribute>();
+        return Task.FromResult(disableBingFeaturesAttribute != null && disableBingFeaturesAttribute.DisableMiddleware);
+    }
+
+    /// <inheritdoc />
+    public abstract Task InvokeAsync(HttpContext context);
+}
