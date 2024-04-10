5f76f64408d66fee06d06e6648466e3c659b8de9
jian玄冰 <jianxuanhuo1@126.com>
2024-04-10 15:01:56 +0800

    ✨ feat(MVC): 新增 异常页过滤器

---
+++ framework/src/Bing.AspNetCore.Mvc/Bing/AspNetCore/Mvc/ExceptionHandling/BingExceptionPageFilter.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+using Bing.AspNetCore.ExceptionHandling;
+using Bing.DependencyInjection;
+using Bing.Http;
+using Bing.Utils.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Bing.ExceptionHandling;
+using Bing.Authorization;
+using Bing.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bing.AspNetCore.Mvc.ExceptionHandling;
+
+/// <summary>
+/// 异常页过滤器
+/// </summary>
+public class BingExceptionPageFilter : IAsyncPageFilter, ITransientDependency
+{
+    /// <inheritdoc />
+    public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context) => Task.CompletedTask;
+
+    /// <summary>
+    /// Razor Page处理程序执行过程中拦截操作。
+    /// </summary>
+    public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+    {
+        if (context.HandlerMethod == null || !ShouldHandleException(context))
+        {
+            await next();
+            return;
+        }
+
+        var pageHandlerExecutedContext = await next();
+        if (pageHandlerExecutedContext.Exception == null)
+            return;
+        await HandleAndWrapException(pageHandlerExecutedContext);
+    }
+
+    /// <summary>
+    /// 是否应该处理异常
+    /// </summary>
+    /// <param name="context">页面处理器执行中上下文</param>
+    protected virtual bool ShouldHandleException(PageHandlerExecutingContext context)
+    {
+        if (context.ActionDescriptor.IsPageAction() && ActionResultHelper.IsObjectResult(context.HandlerMethod!.MethodInfo.ReturnType, typeof(void)))
+            return true;
+        if (context.HttpContext.Request.CanAccept(MimeTypes.Application.Json))
+            return true;
+        if (context.HttpContext.Request.IsAjax())
+            return true;
+        return false;
+    }
+
+    /// <summary>
+    /// 处理并包装异常
+    /// </summary>
+    /// <param name="context">页面处理器执行后上下文</param>
+    protected virtual async Task HandleAndWrapException(PageHandlerExecutedContext context)
+    {
+        if (context.ExceptionHandled)
+            return;
+        var exceptionHandlingOptions = context.GetRequiredService<IOptions<BingExceptionHandlingOptions>>().Value;
+        var exceptionToErrorInfoConverter = context.GetRequiredService<IExceptionToErrorInfoConverter>();
+        var remoteServiceErrorInfo = exceptionToErrorInfoConverter.Convert(context.Exception, options =>
+        {
+            options.SendExceptionDetailsToClients = exceptionHandlingOptions.SendExceptionDetailsToClients;
+            options.SendStackTraceToClients = exceptionHandlingOptions.SendStackTraceToClients;
+        });
+
+        var remoteServiceErrorInfoBuilder = new StringBuilder();
+        remoteServiceErrorInfoBuilder.AppendLine($"---------- {nameof(RemoteServiceErrorInfo)} ----------");
+        remoteServiceErrorInfoBuilder.AppendLine(JsonHelper.ToJson(remoteServiceErrorInfo, indented: true));
+
+        var logger = context.GetService<ILogger<BingExceptionPageFilter>>(NullLogger<BingExceptionPageFilter>.Instance);
+        var logLevel = context.Exception!.GetLogLevel();
+        logger.LogWithLevel(logLevel, remoteServiceErrorInfoBuilder.ToString());
+        logger.LogException(context.Exception!, logLevel);
+
+        await context.GetRequiredService<IExceptionNotifier>().NotifyAsync(new ExceptionNotificationContext(context.Exception!));
+
+        if (context.Exception is BingAuthorizationException)
+        {
+            await context.HttpContext.RequestServices.GetRequiredService<IBingAuthorizationExceptionHandler>()
+                .HandleAsync(context.Exception.As<BingAuthorizationException>(), context.HttpContext);
+        }
+        else
+        {
+            context.HttpContext.Response.Headers.Add(BingHttpConst.BingErrorFormat, "true");
+            context.HttpContext.Response.StatusCode = (int)context
+                .GetRequiredService<IHttpExceptionStatusCodeFinder>()
+                .GetStatusCode(context.HttpContext, context.Exception!);
+
+            context.Result = new ApiResult(Conv.ToInt(remoteServiceErrorInfo.Code), remoteServiceErrorInfo.Message);
+        }
+
+        context.ExceptionHandled = true;
+    }
+}
--- framework/src/Bing.AspNetCore.Mvc/Microsoft/AspNetCore/Mvc/Abstractions/ActionDescriptorExtensions.cs
+++ framework/src/Bing.AspNetCore.Mvc/Microsoft/AspNetCore/Mvc/Abstractions/ActionDescriptorExtensions.cs
@@ -1,7 +1,8 @@
 ﻿using System.Reflection;
+using Bing;
 using Bing.AspNetCore.Mvc;
-using Bing.Exceptions;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Microsoft.AspNetCore.Mvc.Abstractions;
 
@@ -11,37 +12,61 @@ namespace Microsoft.AspNetCore.Mvc.Abstractions;
 public static class ActionDescriptorExtensions
 {
     /// <summary>
-    /// 转换为控制器操作符
+    /// 将<see cref="ActionDescriptor"/>转换为<see cref="ControllerActionDescriptor"/>。
     /// </summary>
     /// <param name="actionDescriptor">操作描述符</param>
+    /// <returns>转换后的<see cref="ControllerActionDescriptor"/>对象。</returns>
+    /// <exception cref="BingFrameworkException">如果actionDescriptor不是ControllerActionDescriptor类型，则抛出异常。</exception>
     public static ControllerActionDescriptor AsControllerActionDescriptor(this ActionDescriptor actionDescriptor)
     {
         if (!actionDescriptor.IsControllerAction())
-            throw new Warning($"{nameof(actionDescriptor)} should be type of {typeof(ControllerActionDescriptor).AssemblyQualifiedName}");
+            throw new BingFrameworkException($"{nameof(actionDescriptor)} should be type of {typeof(ControllerActionDescriptor).AssemblyQualifiedName}");
         return actionDescriptor as ControllerActionDescriptor;
     }
 
     /// <summary>
-    /// 获取方法信息
+    /// 获取与给定的操作描述符关联的方法信息
     /// </summary>
     /// <param name="actionDescriptor">操作描述符</param>
+    /// <returns>表示操作方法的<see cref="MethodInfo"/>对象。</returns>
     public static MethodInfo GetMethodInfo(this ActionDescriptor actionDescriptor) => actionDescriptor.AsControllerActionDescriptor().MethodInfo;
 
     /// <summary>
-    /// 获取返回类型
+    /// 获取指定操作描述符所代表的的方法的返回类型。
     /// </summary>
     /// <param name="actionDescriptor">操作描述符</param>
     public static Type GetReturnType(this ActionDescriptor actionDescriptor) => actionDescriptor.GetMethodInfo().ReturnType;
 
     /// <summary>
-    /// 是否有对象结果
+    /// 判断指定的操作描述符的返回类型是否为对象结果。
     /// </summary>
     /// <param name="actionDescriptor">操作描述符</param>
+    /// <returns>如果操作描述符的返回类型是对象结果（如<see cref="ObjectResult"/>或<see cref="ActionResult{TValue}"/>等），则返回true；否则返回false。</returns>
     public static bool HasObjectResult(this ActionDescriptor actionDescriptor) => ActionResultHelper.IsObjectResult(actionDescriptor.GetReturnType());
 
     /// <summary>
-    /// 是否控制器操作符
+    /// 判断一个操作描述符是否为MVC的控制器操作描述符。
     /// </summary>
     /// <param name="actionDescriptor">操作描述符</param>
     public static bool IsControllerAction(this ActionDescriptor actionDescriptor) => actionDescriptor is ControllerActionDescriptor;
+
+    /// <summary>
+    /// 判断一个操作描述符是否为Razor Page的页面操作描述符。
+    /// </summary>
+    /// <param name="actionDescriptor">操作描述符</param>
+    public static bool IsPageAction(this ActionDescriptor actionDescriptor) => actionDescriptor is PageActionDescriptor;
+
+    /// <summary>
+    /// 将<see cref="ActionDescriptor"/>转换为<see cref="PageActionDescriptor"/>。
+    /// </summary>
+    /// <param name="actionDescriptor">操作描述符</param>
+    /// <returns>转换后的<see cref="PageActionDescriptor"/>对象。</returns>
+    /// <exception cref="BingFrameworkException">如果actionDescriptor不是PageActionDescriptor类型，则抛出异常。</exception>
+    public static PageActionDescriptor AsPageAction(this ActionDescriptor actionDescriptor)
+    {
+        if (!actionDescriptor.IsPageAction())
+            throw new BingFrameworkException($"{nameof(actionDescriptor)} should be type of {typeof(PageActionDescriptor).AssemblyQualifiedName}");
+        return actionDescriptor as PageActionDescriptor;
+    }
+
 }
