ed4d75fc9b36e6b981e146ce6525c96d62de9f45
jian玄冰 <jianxuanhuo1@126.com>
2024-10-15 17:12:22 +0800

    samples: 新增 winform 例子

---
+++ samples/Bing.Samples.Winform/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Bing.Samples.Winform;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Application.Run(new Form1());
+    }
+}
