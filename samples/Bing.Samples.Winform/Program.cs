47e376919ebf3d348fabffff8c158d4e1024fbb3
jian玄冰 <jianxuanhuo1@126.com>
2024-10-17 14:21:04 +0800

    🗑 remove(Dapper): 移除 Dapper 库

--- samples/Bing.Samples.Winform/Program.cs
+++ samples/Bing.Samples.Winform/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Bing.Samples.Winform;
 
@@ -12,6 +14,43 @@ internal static class Program
     {
         ApplicationConfiguration.Initialize();
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-        Application.Run(new Form1());
+        var serviceProvider = GetServiceProvider();
+        using var scope = serviceProvider.CreateScope();
+        Application.Run(scope.ServiceProvider.GetService<Form1>());
+    }
+
+    /// <summary>
+    /// 获取服务提供程序
+    /// </summary>
+    static IServiceProvider GetServiceProvider()
+    {
+        var services = new ServiceCollection();
+        var configuration = InitConfiguration(services);
+        services.AddSingleton(configuration);
+        AddServices(services, configuration);
+        services.AddBing();
+        return services.BuildServiceProvider();
+    }
+
+    /// <summary>
+    /// 初始化配置
+    /// </summary>
+    static IConfiguration InitConfiguration(IServiceCollection services)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddInMemoryCollection()
+            .SetBasePath(AppContext.BaseDirectory)  // 设置为应用程序的执行目录
+            .AddJsonFile("appsettings.json", optional: true);
+        var configuration = builder.Build();
+        services.AddOptions();
+        return configuration;
+    }
+
+    /// <summary>
+    /// 注册服务
+    /// </summary>
+    static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+
     }
 }
