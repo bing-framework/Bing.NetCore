32b9f67752003bd1231b42fb86a32328b4575b75
jian玄冰 <jianxuanhuo1@126.com>
2025-06-03 11:51:44 +0800

    🎨 refactor(Biz): 重构 Addresses、Contacts 和 Enums 模块
    - 将 Addresses、Contacts 和 Enums 模块的代码重新组织到 Bing.Biz 命名空间下
    - 更新了文件结构，使其更加清晰和一致
    - 优化了部分代码格式，提高了可读性

---
+++ components/src/Bing.Biz/Bing/Biz/Addresses/NullAddress.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bing.Biz.Addresses;
+
+/// <summary>
+/// 空地址
+/// </summary>
+[NotMapped]
+public class NullAddress : Address
+{
+}
